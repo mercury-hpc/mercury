bd09683296fffb4cb91270ee848e771d94930b8e
Jerome Soumagne <jsoumagne@hdfgroup.org>
2013-03-05 10:09:03 -0600

    Add explicit definition of test procs

--- test/test_bds.h
+++ test/test_bds.h
@@ -12,10 +12,62 @@
 /* Dummy function that needs to be shipped */
 size_t bla_write(int fildes, const void *buf, size_t nbyte);
 
+#ifdef IOFSL_SHIPPER_HAS_BOOST
 /* Generate processor and struct for required input/output structs
  * IOFSL_SHIPPER_GEN_PROC( struct_type_name, fields )
  */
 IOFSL_SHIPPER_GEN_PROC( bla_write_in_t, ((int32_t)(fildes)) ((bds_handle_t)(bds_handle)) )
 IOFSL_SHIPPER_GEN_PROC( bla_write_out_t, ((uint64_t)(ret)) )
+#else
+/* Define bla_write_in_t */
+typedef struct {
+    int32_t fildes;
+    bds_handle_t bds_handle;
+} bla_write_in_t;
+
+/* Define fs_proc_bla_write_in_t */
+static inline int fs_proc_bla_write_in_t(fs_proc_t proc, void *data)
+{
+    int ret = S_SUCCESS;
+    bla_write_in_t *struct_data = (bla_write_in_t *) data;
+
+    ret = fs_proc_int32_t(proc, &struct_data->fildes);
+    if (ret != S_SUCCESS) {
+        S_ERROR_DEFAULT("Proc error");
+        ret = S_FAIL;
+        return ret;
+    }
+
+    ret = fs_proc_bds_handle_t(proc, &struct_data->bds_handle);
+    if (ret != S_SUCCESS) {
+        S_ERROR_DEFAULT("Proc error");
+        ret = S_FAIL;
+        return ret;
+    }
+
+    return ret;
+}
+
+/* Define bla_write_out_t */
+typedef struct {
+    uint64_t ret;
+} bla_write_out_t;
+
+/* Define fs_proc_bla_write_out_t */
+static inline int fs_proc_bla_write_out_t(fs_proc_t proc, void *data)
+{
+    int ret = S_SUCCESS;
+    bla_write_out_t *struct_data = (bla_write_out_t *) data;
+
+    ret = fs_proc_uint64_t(proc, &struct_data->ret);
+    if (ret != S_SUCCESS) {
+        S_ERROR_DEFAULT("Proc error");
+        ret = S_FAIL;
+        return ret;
+    }
+
+    return ret;
+}
+#endif
 
 #endif /* BDS_TEST_H */
--- test/test_fs.h
+++ test/test_fs.h
@@ -8,6 +8,7 @@
 #include "generic_macros.h"
 #include "generic_proc.h"
 
+#ifdef IOFSL_SHIPPER_HAS_BOOST
 /* 1. Generate processor and struct for additional struct types
  * IOFSL_SHIPPER_GEN_PROC( struct_type_name, fields )
  */
@@ -21,5 +22,85 @@ int bla_open(const char *path, bla_handle_t handle, int *event_id);
  */
 IOFSL_SHIPPER_GEN_PROC( bla_open_in_t, ((fs_string_t)(path)) ((bla_handle_t)(handle)) )
 IOFSL_SHIPPER_GEN_PROC( bla_open_out_t, ((int32_t)(ret)) ((int32_t)(event_id)) )
+#else
+/* Define bla_handle_t */
+typedef struct {
+    uint64_t cookie;
+} bla_handle_t;
+
+/* Define fs_proc_bla_handle_t */
+static inline int fs_proc_bla_handle_t(fs_proc_t proc, void *data)
+{
+    int ret = S_SUCCESS;
+    bla_handle_t *struct_data = (bla_handle_t *) data;
+
+    ret = fs_proc_uint64_t(proc, &struct_data->cookie);
+    if (ret != S_SUCCESS) {
+        S_ERROR_DEFAULT("Proc error");
+        ret = S_FAIL;
+        return ret;
+    }
+
+    return ret;
+}
+
+/* Define bla_open_in_t */
+typedef struct {
+    fs_string_t path;
+    bla_handle_t handle;
+} bla_open_in_t;
+
+/* Define fs_proc_bla_open_in_t */
+static inline int fs_proc_bla_open_in_t(fs_proc_t proc, void *data)
+{
+    int ret = S_SUCCESS;
+    bla_open_in_t *struct_data = (bla_open_in_t *) data;
+
+    ret = fs_proc_fs_string_t(proc, &struct_data->path);
+    if (ret != S_SUCCESS) {
+        S_ERROR_DEFAULT("Proc error");
+        ret = S_FAIL;
+        return ret;
+    }
+
+    ret = fs_proc_bla_handle_t(proc, &struct_data->handle);
+    if (ret != S_SUCCESS) {
+        S_ERROR_DEFAULT("Proc error");
+        ret = S_FAIL;
+        return ret;
+    }
+
+    return ret;
+}
+
+/* Define bla_open_out_t */
+typedef struct {
+    int32_t ret;
+    int32_t event_id;
+} bla_open_out_t;
+
+/* Define fs_proc_bla_open_out_t */
+static inline int fs_proc_bla_open_out_t(fs_proc_t proc, void *data)
+{
+    int ret = S_SUCCESS;
+    bla_open_out_t *struct_data = (bla_open_out_t *) data;
+
+    ret = fs_proc_int32_t(proc, &struct_data->ret);
+    if (ret != S_SUCCESS) {
+        S_ERROR_DEFAULT("Proc error");
+        ret = S_FAIL;
+        return ret;
+    }
+
+    ret = fs_proc_int32_t(proc, &struct_data->event_id);
+    if (ret != S_SUCCESS) {
+        S_ERROR_DEFAULT("Proc error");
+        ret = S_FAIL;
+        return ret;
+    }
+
+    return ret;
+}
+#endif
 
 #endif /* TEST_FS_H */
