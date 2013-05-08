496df5483cad5a02743f32fddf9484935962dce4
Jerome Soumagne <jsoumagne@hdfgroup.org>
2013-05-08 10:00:30 -0500

    Add coverage options and tweak jenkins test

--- Testing/jenkins_mercury.cmake
+++ Testing/jenkins_mercury.cmake
@@ -5,22 +5,14 @@
 #   MERCURY_DO_COVERAGE
 
 # MERCURY_BUILD_CONFIGURATION = Debug | Release
-set(MERCURY_BUILD_CONFIGURATION "$ENV{MERCURY_BUILD_CONFIGURATION}")
-if(NOT MERCURY_BUILD_CONFIGURATION)
-  set(MERCURY_BUILD_CONFIGURATION "Debug")
-endif()
+set(MERCURY_BUILD_CONFIGURATION "Debug")
 
 # MERCURY_DASHBOARD_MODEL=Experimental | Nightly | Continuous
-set(MERCURY_DASHBOARD_MODEL "$ENV{MERCURY_DASHBOARD_MODEL}")
-if(NOT MERCURY_DASHBOARD_MODEL)
-  set(MERCURY_DASHBOARD_MODEL "Experimental")
-endif()
+set(MERCURY_DASHBOARD_MODEL "Continuous")
 
 # Disable loop when MERCURY_DASHBOARD_MODEL=Continuous
-if($ENV{MERCURY_NO_LOOP})
-  message("Disabling looping (if applicable)")
-  set(dashboard_disable_loop TRUE)
-endif()
+message("Disabling looping (if applicable)")
+set(dashboard_disable_loop TRUE)
 
 # Build shared libraries
 set(mercury_build_shared ON)
@@ -29,7 +21,7 @@ string(TOLOWER ${MERCURY_DASHBOARD_MODEL} lower_mercury_dashboard_model)
 string(TOLOWER ${MERCURY_BUILD_CONFIGURATION} lower_mercury_build_configuration)
 set(CTEST_BUILD_CONFIGURATION ${MERCURY_BUILD_CONFIGURATION})
 # Number of jobs to build
-set(CTEST_BUILD_FLAGS "-j2")
+set(CTEST_BUILD_FLAGS "-j4")
 # Build name referenced in cdash
 set(CTEST_BUILD_NAME "jenkins-x64-${lower_mercury_dashboard_model}")
 if($ENV{MERCURY_BUILD_STATIC_LIBRARIES})
@@ -47,17 +39,13 @@ set(CTEST_SITE "$ENV{NODE_NAME}")
 set(CTEST_TEST_TIMEOUT 180) # 3 minute timeout
 
 # Optional coverage options
-set(cov_options)
-if($ENV{MERCURY_DO_COVERAGE})
+set(dashboard_do_coverage ON)
+if(dashboard_do_coverage)
   message("Enabling Coverage")
   set(CTEST_COVERAGE_COMMAND "/usr/bin/gcov")
-  set(cov_options "-fprofile-arcs -ftest-coverage")
   set(CTEST_BUILD_NAME "${CTEST_BUILD_NAME}-coverage")
   # don't run parallel coverage tests, no matter what.
-  set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
-
-  # needed by mercury_common.cmake 
-  set(dashboard_do_coverage TRUE)
+  # set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
 
   # add Coverage dir to the root so that we don't mess the non-coverage
   # dashboard.
@@ -73,18 +61,17 @@ set (dashboard_model ${MERCURY_DASHBOARD_MODEL})
 
 # Initial cache used to build mercury, options can be modified here
 set(dashboard_cache "
-CMAKE_C_FLAGS:STRING=-Wall -Wextra -Wshadow ${cov_options}
-CMAKE_EXE_LINKER_FLAGS:STRING=${cov_options}
-CMAKE_SHARED_LINKER_FLAGS:STRING=${cov_options}
+CMAKE_C_FLAGS:STRING=-Wall -Wextra -Wshadow
 
 BUILD_SHARED_LIBS:BOOL=${mercury_build_shared}
 BUILD_TESTING:BOOL=ON
 
+MERCURY_ENABLE_COVERAGE:BOOL=${dashboard_do_coverage}
 MERCURY_USE_BOOST_PP:BOOL=OFF
 MERCURY_USE_XDR:BOOL=OFF
 NA_USE_BMI:BOOL=OFF
 NA_USE_MPI:BOOL=ON
-MPIEXEC_MAX_NUMPROCS:STRING=2
+MPIEXEC_MAX_NUMPROCS:STRING=4
 ")
 
 set(dashboard_git_url $ENV{GIT_URL})
