18d64525524992189420d870acdb1bc869e7554b
Sumit Narayan <sumit@mcs.anl.gov>
2013-10-14 15:50:21 -0500

    - Changes required to run bmi tests in jenkins.

--- Testing/jenkins_build.sh
+++ Testing/jenkins_build.sh
@@ -3,6 +3,23 @@
 echo "Running build script from repository"
 echo "(current dir is repo root: $PWD)"
 set
-#cmake . && make
+# store the current directory in a local variable to get back to it later
+MERCURY_WORKSPACE_DIR=`pwd`
+
+# clone bmi's git repository
+git clone git://git.mcs.anl.gov/bmi $WORKSPACE/bmi
+
+# build bmi
+cd $WORKSPACE/bmi
+./prepare && CFLAGS=-fPIC ./configure --prefix=$WORKSPACE/.install
+make && make install
+
+# get back to the location where we were at the begining
+cd $MERCURY_WORKSPACE_DIR
+
+# export variable needed for bmi testing
+export MERCURY_PORT_NAME='tcp://localhost:3344'
+
+# configure, build and test
 ctest -S $PWD/Testing/jenkins_mercury.cmake -VV 2>&1
 
