mercury-hpc/mercury

commit 60f03f0c14e890edc898be3c326a10045f2a7940

- Resolves an issue where transfer of data >4K was causing a   segmentation fault, because HG_Bulk was not initialized.

src/mercury.c
@@ -15,6 +15,7 @@
 #include "mercury_hash_string.h"
 #include "mercury_thread.h"
 #include "mercury_thread_mutex.h"
+#include "mercury_bulk.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -408,6 +409,19 @@ HG_Init(na_class_t *network_class)
 
     hg_na_class = network_class;
 
+    /* Initialize bulk module */
+    /* TODO: This code may have to be changed in accordance with the
+     *       outcome of Trac#24.
+     */
+    ret = HG_Bulk_init(network_class);
+    if (ret != HG_SUCCESS)
+    {
+        /* Let us assume that we are using the same network class
+         * for initializing when used as a server and a client
+         */
+        ret = HG_SUCCESS;
+    }
+    
     /* Initialize mutex for tags */
     hg_thread_mutex_init(&tag_mutex);
 

src/mercury_handler.c
@@ -377,6 +377,19 @@ HG_Handler_init(na_class_t *network_class)
 
     handler_na_class = network_class;
 
+    /* Initialize bulk module */
+    /* TODO: This code may have to be changed in accordance with the
+     *       outcome of Trac#24.
+     */
+    ret = HG_Bulk_init(network_class);
+    if (ret != HG_SUCCESS)
+    {
+        /* Let us assume that we are using the same network class
+         * for initializing when used as a server and a client
+         */
+        ret = HG_SUCCESS;
+    }
+
     /* Create new function map */
     handler_func_map = hg_hash_table_new(hg_int_hash, hg_int_equal);
     if (!handler_func_map) {

src/mercury_proc_header.c
@@ -113,6 +113,7 @@ hg_proc_header_request(hg_proc_t proc, hg_header_request_t *header)
         hg_proc_hg_bulk_t(proc, &header->extra_buf_handle);
     } else if (hg_proc_get_op(proc) == HG_DECODE) {
         header->extra_buf_handle = HG_BULK_NULL;
+        hg_proc_hg_bulk_t(proc, &header->extra_buf_handle);
     }
 
     if (hg_proc_get_op(proc) == HG_DECODE) {

commit e47cd00e8dd63f82ac1a44a230412c8da10563e2

- Commit 60f03f0c14e890edc898be3c326a10045f2a7940 caused all test code   to fail.  This fix corrects the fault introduced by that commit.

src/mercury.c
@@ -410,16 +410,12 @@ HG_Init(na_class_t *network_class)
     hg_na_class = network_class;
 
     /* Initialize bulk module */
-    /* TODO: This code may have to be changed in accordance with the
-     *       outcome of Trac#24.
-     */
     ret = HG_Bulk_init(network_class);
     if (ret != HG_SUCCESS)
     {
-        /* Let us assume that we are using the same network class
-         * for initializing when used as a server and a client
-         */
-        ret = HG_SUCCESS;
+        HG_ERROR_DEFAULT("Error initializing bulk module.");
+        ret = HG_FAIL;
+        return ret;
     }
     
     /* Initialize mutex for tags */

src/mercury_bulk.c
@@ -54,9 +54,12 @@ HG_Bulk_init(na_class_t *network_class)
         return ret;
     }
 
+    /* TODO: This code may have to be changed in accordance with the
+     *       outcome of Trac#24.
+     */
     if (bulk_na_class) {
-        HG_ERROR_DEFAULT("Already initialized");
-        ret = HG_FAIL;
+        HG_WARNING_DEFAULT("Already initialized");
+        ret = HG_SUCCESS;
         return ret;
     }
 

src/mercury_error.h
@@ -33,8 +33,15 @@
                 "%s.\n",                          \
                 __FILE__, __LINE__, __func__, x); \
   }
+  #define HG_WARNING_DEFAULT(x) {                 \
+        fprintf(stderr, "Warning "                \
+              "in %s:%d (%s): "                   \
+              "%s.\n",                            \
+              __FILE__, __LINE__, __func__, x);   \
+  }
 #else
   #define HG_ERROR_DEFAULT(x)
+  #define HG_WARNING_DEFAULT(x)
 #endif
 
 #endif /* MERCURY_ERROR_H */

src/mercury_handler.c
@@ -378,16 +378,12 @@ HG_Handler_init(na_class_t *network_class)
     handler_na_class = network_class;
 
     /* Initialize bulk module */
-    /* TODO: This code may have to be changed in accordance with the
-     *       outcome of Trac#24.
-     */
     ret = HG_Bulk_init(network_class);
     if (ret != HG_SUCCESS)
     {
-        /* Let us assume that we are using the same network class
-         * for initializing when used as a server and a client
-         */
-        ret = HG_SUCCESS;
+        HG_ERROR_DEFAULT("Error initializing bulk module.");
+        ret = HG_FAIL;
+        goto done;
     }
 
     /* Create new function map */