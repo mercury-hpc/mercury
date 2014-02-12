f4b2df443596b6729455a99b29025a94988f41a4
Jerome Soumagne <jsoumagne@hdfgroup.org>
2014-02-12 15:21:06 -0600

    Fix FindOPA.cmake and pick up OPA using pkg-config

--- src/util/CMake/FindOPA.cmake
+++ src/util/CMake/FindOPA.cmake
@@ -1,38 +1,25 @@
-#  Try to find OPA library and headers.
-#  This file sets the following variables:
-#
-#  OPA_INCLUDE_DIR, where to find opa_primitives.h, etc.
-#  OPA_LIBRARIES, the libraries to link against
-#  OPA_FOUND, If false, do not try to use OPA.
-#
-# Also defined, but not for general use are:
-#  OPA_LIBRARY, the full path to the opa library.
+# - Try to find OPA
+# Once done this will define
+#  OPA_FOUND - System has OpenPA
+#  OPA_INCLUDE_DIRS - The OPA include directories
+#  OPA_LIBRARIES - The libraries needed to use OPA
 
-FIND_PATH( OPA_INCLUDE_DIR opa_primitives.h
-  /usr/local/include
-  /usr/include
-)
+find_package(PkgConfig)
+pkg_check_modules(PC_OPA QUIET openpa)
 
-FIND_LIBRARY( OPA_LIBRARY NAMES opa
-  /usr/lib
-  /usr/local/lib
-)
+find_path(OPA_INCLUDE_DIR opa_primitives.h
+  HINTS ${PC_OPA_INCLUDEDIR} ${PC_OPA_INCLUDE_DIRS})
 
-SET( OPA_FOUND "NO" )
-IF(OPA_INCLUDE_DIR)
-  IF(OPA_LIBRARY)
+find_library(OPA_LIBRARY NAMES opa libopa
+  HINTS ${PC_OPA_LIBDIR} ${PC_OPA_LIBRARY_DIRS})
 
-    SET( OPA_LIBRARIES ${OPA_LIBRARY})
-    SET( OPA_FOUND "YES" )
+set(OPA_LIBRARIES ${OPA_LIBRARY})
+set(OPA_INCLUDE_DIRS ${OPA_INCLUDE_DIR})
 
-  ELSE(OPA_LIBRARY)
-    IF(OPA_FIND_REQUIRED)
-      message(SEND_ERROR "Unable to find the requested OPA libraries.")
-    ENDIF(OPA_FIND_REQUIRED)
-  ENDIF(OPA_LIBRARY)
-ENDIF(OPA_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set OPA_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(OPA DEFAULT_MSG
+                                  OPA_LIBRARY OPA_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(
-  OPA_INCLUDE_DIR
-  OPA_LIBRARY
-)
+mark_as_advanced(OPA_INCLUDE_DIR OPA_LIBRARY)
