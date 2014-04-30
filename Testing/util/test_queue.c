1d8dde03453c8db3bf3716c23b504dd471f9a9d3
Jerome Soumagne <jsoumagne@hdfgroup.org>
2014-04-29 23:32:43 -0500

    Add mercury util hash_table/queue/time tests

--- Testing/util/test_queue.c
+++ Testing/util/test_queue.c
@@ -8,16 +8,62 @@
 int
 main(int argc, char *argv[])
 {
-    hg_util_int32_t value = 0;
+    hg_queue_t *queue = NULL;
     int ret = EXIT_SUCCESS;
+    int value1 = 10, value2 = 20;
 
     (void) argc;
     (void) argv;
 
+    queue = hg_queue_new();
 
-    if (value != 100) {
-        fprintf(stderr, "Error: atomic value is %d\n", value);
+    if (!hg_queue_is_empty(queue)) {
+        fprintf(stderr, "Error: queue should be empty\n");
         ret = EXIT_FAILURE;
+        goto done;
     }
+
+    hg_queue_push_head(queue, (hg_queue_value_t) &value1);
+
+    if (value1 != *((int *) hg_queue_peek_head(queue))) {
+        fprintf(stderr, "Error: values do not match\n");
+        ret = EXIT_FAILURE;
+        goto done;
+    }
+
+    if (hg_queue_is_empty(queue)) {
+        fprintf(stderr, "Error: queue should not be empty\n");
+        ret = EXIT_FAILURE;
+        goto done;
+    }
+
+    hg_queue_push_tail(queue, (hg_queue_value_t) &value2);
+
+    if (value2 != *((int *) hg_queue_peek_tail(queue))) {
+        fprintf(stderr, "Error: values do not match\n");
+        ret = EXIT_FAILURE;
+        goto done;
+    }
+
+    if (value2 != *((int *) hg_queue_pop_tail(queue))) {
+        fprintf(stderr, "Error: values do not match\n");
+        ret = EXIT_FAILURE;
+        goto done;
+    }
+
+    if (value1 != *((int *) hg_queue_pop_head(queue))) {
+        fprintf(stderr, "Error: values do not match\n");
+        ret = EXIT_FAILURE;
+        goto done;
+    }
+
+    if (!hg_queue_is_empty(queue)) {
+        fprintf(stderr, "Error: queue should be empty\n");
+        ret = EXIT_FAILURE;
+        goto done;
+    }
+
+done:
+    hg_queue_free(queue);
     return ret;
 }
--- Testing/util/test_time.c
+++ Testing/util/test_time.c
@@ -8,16 +8,59 @@
 int
 main(int argc, char *argv[])
 {
-    hg_util_int32_t value = 0;
+    hg_time_t t1, t2, diff1, diff2;
+    hg_time_t sleep_time = {1, 0};
+    double t1_double, t2_double;
     int ret = EXIT_SUCCESS;
 
     (void) argc;
     (void) argv;
 
+    printf("Current time: %s\n", hg_time_stamp());
 
-    if (value != 100) {
-        fprintf(stderr, "Error: atomic value is %d\n", value);
+    hg_time_get_current(&t1);
+
+    hg_time_sleep(sleep_time, NULL);
+
+    hg_time_get_current(&t2);
+
+    /* Should have slept at least sleep_time */
+    if (!hg_time_less(t1, t2)) {
+        fprintf(stderr, "Error: t1 > t2\n");
         ret = EXIT_FAILURE;
+        goto done;
     }
+
+    t1_double = hg_time_to_double(t1);
+    t2_double = hg_time_to_double(t2);
+
+    if (t1_double > t2_double) {
+        fprintf(stderr, "Error: t1 > t2\n");
+        ret = EXIT_FAILURE;
+        goto done;
+    }
+
+    t1 = hg_time_from_double(t1_double);
+    t2 = hg_time_from_double(t2_double);
+
+    if (!hg_time_less(t1, t2)) {
+        fprintf(stderr, "Error: t1 > t2\n");
+        ret = EXIT_FAILURE;
+        goto done;
+    }
+
+    /* t2 - (t1 + sleep_time) */
+    diff1 = hg_time_subtract(t2, hg_time_add(t1, sleep_time));
+    /* (t2 - t1) - sleep_time */
+    diff2 = hg_time_subtract(hg_time_subtract(t2, t1), sleep_time);
+
+    /* Should be equal */
+    if (hg_time_to_double(diff1) != hg_time_to_double(diff2)) {
+        fprintf(stderr, "Error: diff1 != diff2\n");
+        ret = EXIT_FAILURE;
+        goto done;
+    }
+
+done:
     return ret;
 }
