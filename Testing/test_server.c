c692e8dcac371dfe0c6dfc7ff8f65e8af32f260f
Jerome Soumagne <jsoumagne@hdfgroup.org>
2014-09-12 16:57:33 -0500

    Fix test server

--- Testing/test_server.c
+++ Testing/test_server.c
@@ -25,7 +25,6 @@ main(int argc, char *argv[])
 {
     hg_class_t *hg_class = NULL;
     hg_context_t *context = NULL;
-    hg_bool_t finalizing = HG_FALSE;
     unsigned int number_of_peers;
     hg_return_t ret = HG_SUCCESS;
 
@@ -34,23 +33,26 @@ main(int argc, char *argv[])
 
     context = HG_Context_create(hg_class);
 
-    while (!finalizing) {
-        hg_return_t trigger_ret;
+    while (1) {
         unsigned int actual_count = 0;
 
         do {
-            trigger_ret = HG_Trigger(hg_class, context, 0, 1, &actual_count);
-        } while ((trigger_ret == HG_SUCCESS) && actual_count);
+            ret = HG_Trigger(hg_class, context, 0, 1, &actual_count);
+        } while ((ret == HG_SUCCESS) && actual_count);
 
         if (hg_atomic_cas32(&hg_test_finalizing_count_g, 1, 0))
-            finalizing = HG_TRUE;
+            break;
 
         HG_Progress(hg_class, context, HG_MAX_IDLE_TIME);
     }
 
     printf("# Finalizing...\n");
 
-    HG_Context_destroy(context);
+    ret = HG_Context_destroy(context);
+    if (ret != HG_SUCCESS) {
+        fprintf(stderr, "Could not destroy context\n");
+    }
+
     HG_Test_finalize(hg_class);
 
     return EXIT_SUCCESS;
