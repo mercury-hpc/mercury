4f2fda5379fa906f9b530841e408895c1a150103
Jerome Soumagne <jsoumagne@hdfgroup.org>
2014-11-06 13:30:13 -0600

    Tweak test server

--- Testing/test_server.c
+++ Testing/test_server.c
@@ -11,11 +11,32 @@
 #include "mercury_test.h"
 
 #include "mercury_atomic.h"
+#include "mercury_thread.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 extern hg_atomic_int32_t hg_test_finalizing_count_g;
+static hg_class_t *hg_class = NULL;
+static hg_context_t *context = NULL;
+
+static HG_THREAD_RETURN_TYPE
+hg_test_server_trigger(void *arg)
+{
+    hg_thread_ret_t thread_ret = (hg_thread_ret_t) 0;
+    unsigned int actual_count = 0;
+    hg_return_t ret = HG_SUCCESS;
+
+    do {
+        if (hg_atomic_cas32(&hg_test_finalizing_count_g, 1, 1))
+            break;
+
+        ret = HG_Trigger(hg_class, context, HG_MAX_IDLE_TIME, 1, &actual_count);
+    } while ((ret == HG_SUCCESS) && actual_count);
+
+    hg_thread_exit(thread_ret);
+    return thread_ret;
+}
 
 /**
  *
@@ -23,29 +44,25 @@ extern hg_atomic_int32_t hg_test_finalizing_count_g;
 int
 main(int argc, char *argv[])
 {
-    hg_class_t *hg_class = NULL;
-    hg_context_t *context = NULL;
+    hg_thread_t thread;
     unsigned int number_of_peers;
     hg_return_t ret = HG_SUCCESS;
 
     hg_class = HG_Test_server_init(argc, argv, NULL, NULL,
             &number_of_peers, &context);
 
-    while (1) {
-        unsigned int actual_count = 0;
+    hg_thread_create(&thread, hg_test_server_trigger, NULL);
 
-        do {
-            ret = HG_Trigger(hg_class, context, 0, 1, &actual_count);
-        } while ((ret == HG_SUCCESS) && actual_count);
-
-        if (hg_atomic_cas32(&hg_test_finalizing_count_g, 1, 0))
+    do {
+        if (hg_atomic_cas32(&hg_test_finalizing_count_g, 1, 1))
             break;
 
-        HG_Progress(hg_class, context, HG_MAX_IDLE_TIME);
-    }
+        ret = HG_Progress(hg_class, context, HG_MAX_IDLE_TIME);
+    } while (ret == HG_SUCCESS);
+
+    hg_thread_join(thread);
 
     printf("# Finalizing...\n");
-
     HG_Test_finalize(hg_class);
 
     return EXIT_SUCCESS;
