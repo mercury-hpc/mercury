afb5cdd2cc6e7f4e705e8d388b0e4a7c3a259f86
Kevin Harms <harms@alcf.anl.gov>
2014-12-05 15:48:04 -0600

    Cancel now works correctly for implemented operations

--- src/na/na_bmi.c
+++ src/na/na_bmi.c
@@ -38,6 +38,9 @@
 #define NA_BMI_PRIVATE_DATA(na_class) \
     ((struct na_bmi_private_data *)(na_class->private_data))
 
+#define NA_BMI_CANCEL_R (1 << 0)
+#define NA_BMI_CANCEL_C (1 << 1)
+
 /************************************/
 /* Local Type and Struct Definition */
 /************************************/
@@ -114,7 +117,6 @@ struct na_bmi_info_get {
     struct na_bmi_rma_info *rma_info;
 };
 
-/* na_bmi_op_id  TODO uint64_t cookie for cancel ?*/
 struct na_bmi_op_id {
     na_context_t *context;
     na_cb_type_t type;
@@ -130,6 +132,7 @@ struct na_bmi_op_id {
       struct na_bmi_info_put put;
       struct na_bmi_info_get get;
     } info;
+    uint64_t cancel;
 };
 
 struct na_bmi_private_data {
@@ -796,6 +799,7 @@ na_bmi_addr_lookup(na_class_t NA_UNUSED *na_class, na_context_t *context,
     na_bmi_op_id->callback = callback;
     na_bmi_op_id->arg = arg;
     na_bmi_op_id->completed = NA_FALSE;
+    na_bmi_op_id->cancel = 0;
 
     /* Allocate addr */
     na_bmi_addr = (struct na_bmi_addr *) malloc(sizeof(struct na_bmi_addr));
@@ -987,6 +991,7 @@ na_bmi_msg_send_unexpected(na_class_t NA_UNUSED *na_class,
     na_bmi_op_id->arg = arg;
     na_bmi_op_id->completed = NA_FALSE;
     na_bmi_op_id->info.send_unexpected.op_id = 0;
+    na_bmi_op_id->cancel = 0;
 
     /* Post the BMI unexpected send request */
     bmi_ret = BMI_post_sendunexpected(
@@ -1043,6 +1048,7 @@ na_bmi_msg_recv_unexpected(na_class_t *na_class, na_context_t *context,
     na_bmi_op_id->info.recv_unexpected.buf = buf;
     na_bmi_op_id->info.recv_unexpected.buf_size = (bmi_size_t) buf_size;
     na_bmi_op_id->info.recv_unexpected.unexpected_info = NULL;
+    na_bmi_op_id->cancel = 0;
 
     /* Try to make progress here from the BMI unexpected queue */
     do {
@@ -1208,6 +1214,7 @@ na_bmi_msg_send_expected(na_class_t NA_UNUSED *na_class, na_context_t *context,
     na_bmi_op_id->arg = arg;
     na_bmi_op_id->completed = NA_FALSE;
     na_bmi_op_id->info.send_expected.op_id = 0;
+    na_bmi_op_id->cancel = 0;
 
     /* Post the BMI send request */
     bmi_ret = BMI_post_send(&na_bmi_op_id->info.send_expected.op_id,
@@ -1267,6 +1274,7 @@ na_bmi_msg_recv_expected(na_class_t NA_UNUSED *na_class, na_context_t *context,
     na_bmi_op_id->info.recv_expected.op_id = 0;
     na_bmi_op_id->info.recv_expected.buf_size = bmi_buf_size;
     na_bmi_op_id->info.recv_expected.actual_size = 0;
+    na_bmi_op_id->cancel = 0;
 
     /* Post the BMI recv request */
     bmi_ret = BMI_post_recv(&na_bmi_op_id->info.recv_expected.op_id,
@@ -1463,6 +1471,7 @@ na_bmi_put(na_class_t *na_class, na_context_t *context, na_cb_t callback,
     na_bmi_op_id->info.put.internal_progress = NA_FALSE;
     na_bmi_op_id->info.put.remote_addr = na_bmi_addr->bmi_addr;
     na_bmi_op_id->info.put.rma_info = NULL;
+    na_bmi_op_id->cancel = 0;
 
     /* Allocate rma info (use calloc to avoid uninitialized transfer) */
     na_bmi_rma_info =
@@ -1580,6 +1589,7 @@ na_bmi_get(na_class_t *na_class, na_context_t *context, na_cb_t callback,
     na_bmi_op_id->info.get.internal_progress = NA_FALSE;
     na_bmi_op_id->info.get.remote_addr = na_bmi_addr->bmi_addr;
     na_bmi_op_id->info.get.rma_info = NULL;
+    na_bmi_op_id->cancel = 0;
 
     /* Allocate rma info (use calloc to avoid uninitialized transfer) */
     na_bmi_rma_info =
@@ -1800,10 +1810,16 @@ na_bmi_progress_expected(na_class_t NA_UNUSED *na_class, na_context_t *context,
     }
 
     if (outcount && na_bmi_op_id) {
-        if (error_code != 0) {
+        if ((error_code != 0) &&
+            (error_code != -BMI_ECANCEL)) {
             NA_LOG_ERROR("BMI_testcontext failed, error code set");
             ret = NA_PROTOCOL_ERROR;
             goto done;
+        }
+
+        if (error_code == -BMI_ECANCEL)
+        {
+            na_bmi_op_id->cancel |= NA_BMI_CANCEL_C;
         }
 
         switch (na_bmi_op_id->type) {
@@ -1941,6 +1957,7 @@ na_bmi_progress_rma(na_class_t NA_UNUSED *na_class, na_context_t *context,
             na_bmi_op_id->info.put.internal_progress = NA_TRUE;
             na_bmi_op_id->info.put.remote_addr = unexpected_info->addr;
             na_bmi_op_id->info.put.rma_info = na_bmi_rma_info;
+            na_bmi_op_id->cancel = 0;
 
             /* Start receiving data */
             bmi_ret = BMI_post_recv(&na_bmi_op_id->info.put.transfer_op_id,
@@ -1971,6 +1988,7 @@ na_bmi_progress_rma(na_class_t NA_UNUSED *na_class, na_context_t *context,
             na_bmi_op_id->info.get.internal_progress = NA_TRUE;
             na_bmi_op_id->info.get.remote_addr = unexpected_info->addr;
             na_bmi_op_id->info.get.rma_info = na_bmi_rma_info;
+            na_bmi_op_id->cancel = 0;
 
             /* Start sending data */
             bmi_ret = BMI_post_send(&na_bmi_op_id->info.get.transfer_op_id,
@@ -2068,7 +2086,7 @@ na_bmi_complete(struct na_bmi_op_id *na_bmi_op_id)
         goto done;
     }
     callback_info->arg = na_bmi_op_id->arg;
-    callback_info->ret = ret;
+    callback_info->ret = ((na_bmi_op_id->cancel & NA_BMI_CANCEL_R) ? NA_CANCELED : ret);
     callback_info->type = na_bmi_op_id->type;
 
     switch (na_bmi_op_id->type) {
@@ -2181,9 +2199,6 @@ na_bmi_cancel(na_class_t *na_class, na_context_t *context, na_op_id_t op_id)
     na_return_t ret = NA_SUCCESS;
     int bmi_ret;
 
-    /* TODO make this atomic */
-    if (na_bmi_op_id->completed) goto done;
-
     switch (na_bmi_op_id->type) {
         case NA_CB_LOOKUP:
             /* Nothing for now */
@@ -2196,6 +2211,7 @@ na_bmi_cancel(na_class_t *na_class, na_context_t *context, na_op_id_t op_id)
                 ret = NA_PROTOCOL_ERROR;
                 goto done;
             }
+            na_bmi_op_id->cancel = NA_BMI_CANCEL_R;
             break;
         case NA_CB_RECV_UNEXPECTED:
         {
@@ -2209,6 +2225,10 @@ na_bmi_cancel(na_class_t *na_class, na_context_t *context, na_op_id_t op_id)
                 if (na_bmi_pop_op_id != na_bmi_op_id) {
                     na_bmi_msg_unexpected_op_push(na_class, na_bmi_pop_op_id);
                 }
+                else
+                {
+                    na_bmi_op_id->cancel = NA_BMI_CANCEL_R;
+                }
             }
         }
             break;
@@ -2220,6 +2240,7 @@ na_bmi_cancel(na_class_t *na_class, na_context_t *context, na_op_id_t op_id)
                 ret = NA_PROTOCOL_ERROR;
                 goto done;
             }
+            na_bmi_op_id->cancel = NA_BMI_CANCEL_R;
             break;
         case NA_CB_RECV_EXPECTED:
             bmi_ret = BMI_cancel(na_bmi_op_id->info.recv_expected.op_id,
@@ -2229,19 +2250,21 @@ na_bmi_cancel(na_class_t *na_class, na_context_t *context, na_op_id_t op_id)
                 ret = NA_PROTOCOL_ERROR;
                 goto done;
             }
+            na_bmi_op_id->cancel = NA_BMI_CANCEL_R;
             break;
         case NA_CB_PUT:
             /* TODO */
+            na_bmi_op_id->cancel = NA_BMI_CANCEL_R;
             break;
         case NA_CB_GET:
             /* TODO */
+            na_bmi_op_id->cancel = NA_BMI_CANCEL_R;
             break;
         default:
             NA_LOG_ERROR("Operation not supported");
             ret = NA_INVALID_PARAM;
             break;
     }
-    free(na_bmi_op_id);
 
 done:
     return ret;
