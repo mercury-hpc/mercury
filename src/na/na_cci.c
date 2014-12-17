12a3cbb7bcbb46353f512765df8120582250978a
Scott Atchley <atchleyes@ornl.gov>
2014-12-17 09:07:06 -0500

    na_cci: use properly scoped variable
    When canceling expected receives, I mistakenly had a duplicate
    variable. Remove the duplicate.
    
    Also, remove unused variable.

--- src/na/na_cci.c
+++ src/na/na_cci.c
@@ -2055,7 +2055,6 @@ na_cci_cancel(na_class_t NA_UNUSED * na_class, na_context_t NA_UNUSED * context,
 	      na_op_id_t NA_UNUSED op_id)
 {
 	na_cci_op_id_t		*na_cci_op_id = (na_cci_op_id_t *)op_id;
-	na_cci_private_data_t	*priv = na_class->private_data;
 	na_cci_addr_t		*na_cci_addr = NULL;
 	na_return_t		ret = NA_SUCCESS;
 
@@ -2105,8 +2104,8 @@ na_cci_cancel(na_class_t NA_UNUSED * na_class, na_context_t NA_UNUSED * context,
 		{
 			int found = 0;
 			na_cci_op_id_t *tmp = NULL;
-			na_cci_addr_t *na_cci_addr =
-				na_cci_op_id->info.recv_expected.na_cci_addr;
+
+			na_cci_addr = na_cci_op_id->info.recv_expected.na_cci_addr;
 
 			/* we can't decref in handle_recv_expected() */
 			op_id_decref(na_cci_op_id);
