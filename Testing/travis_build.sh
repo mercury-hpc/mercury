10927e56a0ee6d9d82c757c13b9b4eccbdf8ecc2
Jerome Soumagne <jsoumagne@hdfgroup.org>
2015-07-15 23:20:53 -0500

    Add cci in travis testing

--- Testing/travis_build.sh
+++ Testing/travis_build.sh
@@ -5,19 +5,12 @@ git clone git://git.mcs.anl.gov/bmi bmi
 cd bmi && ./prepare && ./configure --enable-shared --enable-bmi-only --prefix=/usr && make && sudo make install
 
 # echo mpi commands needed to compile
-echo "mpicc -show"
+echo "which mpicc"
 which mpicc
+echo "mpicc -show"
 mpicc -show
 
 # build cci
-#CCI_VERSION=master
-#mkdir cci
-#pushd cci
-#cp /homes/soumagne/jenkins/cci-${CCI_VERSION}.tar.gz .
-#tar -xzf cci-${CCI_VERSION}.tar.gz
-#pushd cci-${CCI_VERSION}
-#./configure --prefix=$WORKSPACE/.install --enable-static
-#make && make install
-#popd
-#popd
+git clone --branch=v0.2.0 https://github.com/CCI/cci.git
+cd cci && ./autogen.pl && ./configure --prefix=/usr && make && sudo make install
 
