4e8f1fbbe9b1c2318a189392dc28cc25eca77226
Jerome Soumagne <jsoumagne@hdfgroup.org>
2015-07-15 22:25:41 -0500

    Add travis configuration

---
+++ Testing/travis_build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# build bmi
+git clone git://git.mcs.anl.gov/bmi bmi
+pushd bmi
+./prepare && ./configure --enable-shared --enable-bmi-only --prefix=/usr && make && sudo make install
+popd
+
+# echo mpi commands needed to compile
+echo "mpicc -show"
+which mpicc
+mpicc -show
+
+# build cci
+#CCI_VERSION=master
+#mkdir cci
+#pushd cci
+#cp /homes/soumagne/jenkins/cci-${CCI_VERSION}.tar.gz .
+#tar -xzf cci-${CCI_VERSION}.tar.gz
+#pushd cci-${CCI_VERSION}
+#./configure --prefix=$WORKSPACE/.install --enable-static
+#make && make install
+#popd
+#popd
+
