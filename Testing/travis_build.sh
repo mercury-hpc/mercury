b7854fd3aec6ab8e25e85d8038c42127075a57e1
Jerome Soumagne <jsoumagne@hdfgroup.org>
2015-07-19 17:28:22 -0500

    Fix travis_build.sh script

--- Testing/travis_build.sh
+++ Testing/travis_build.sh
@@ -9,11 +9,11 @@ if [ ! -d "$HOME/install/bin" ]; then
   cd bmi && ./prepare && ./configure --enable-shared --enable-bmi-only --prefix=$HOME/install && make && make install;
   # build mpi
   cd $HOME && wget http://www.mpich.org/static/downloads/3.1.4/mpich-3.1.4.tar.gz;
-  tar -xzvf mpich-3.1.4.tar.gz;
-  cd mpich-3.1.4 && ./configure --prefix=$HOME/install && make && make install;
+  tar -xzf mpich-3.1.4.tar.gz;
+  cd mpich-3.1.4 && ./configure --disable-fortran --prefix=$HOME/install && make && make install;
   # build cci
   cd $HOME && wget http://cci-forum.com/wp-content/uploads/2015/07/cci-0.2.0.tar.gz;
-  tar -xzvf cci-0.2.0.tar.gz;
+  tar -xzf cci-0.2.0.tar.gz;
   cd cci-0.2.0 && ./configure --prefix=$HOME/install && make && make install;
 else
   echo "Using cached directory";
