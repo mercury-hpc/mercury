0008ea01e2974d4a0f1a2a0d0d66d37b2f799c19
Jerome Soumagne <jsoumagne@hdfgroup.org>
2016-02-08 16:20:13 -0600

    Update travis build with latest MPI/CCI

--- Testing/travis_build.sh
+++ Testing/travis_build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+MPI_VERSION=3.2
+CCI_VERSION=0.3.0
 
 set -e
 
@@ -8,13 +10,13 @@ if [ ! -d "$HOME/install/bin" ]; then
   cd $HOME && git clone git://git.mcs.anl.gov/bmi bmi;
   cd bmi && ./prepare && ./configure --enable-shared --enable-bmi-only --prefix=$HOME/install && make && make install;
   # build mpi
-  cd $HOME && wget http://www.mpich.org/static/downloads/3.1.4/mpich-3.1.4.tar.gz;
-  tar -xzf mpich-3.1.4.tar.gz;
-  cd mpich-3.1.4 && ./configure --disable-fortran --prefix=$HOME/install && make && make install;
+  cd $HOME && wget http://www.mpich.org/static/downloads/${MPI_VERSION}/mpich-${MPI_VERSION}.tar.gz;
+  tar -xzf mpich-${MPI_VERSION}.tar.gz;
+  cd mpich-${MPI_VERSION} && ./configure --disable-fortran --prefix=$HOME/install && make && make install;
   # build cci
-  cd $HOME && wget http://cci-forum.com/wp-content/uploads/2015/07/cci-0.2.0.tar.gz;
-  tar -xzf cci-0.2.0.tar.gz;
-  cd cci-0.2.0 && ./configure --prefix=$HOME/install && make && make install;
+  cd $HOME && wget http://cci-forum.com/wp-content/uploads/2015/12/cci-${CCI_VERSION}.tar.gz
+  tar -xzf cci-${CCI_VERSION}.tar.gz;
+  cd cci-${CCI_VERSION} && ./configure --prefix=$HOME/install && make && make install;
 else
   echo "Using cached directory";
 fi
