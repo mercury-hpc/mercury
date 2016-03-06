f08968ba2012e788463a0f228a5a42db4f4c0e0d
Phil Carns <carns@mcs.anl.gov>
2016-03-06 00:22:13 -0500

    use short sends for req/response

--- src/na/na_bmi.c
+++ src/na/na_bmi.c
@@ -2122,9 +2122,9 @@ na_bmi_complete(struct na_bmi_op_id *na_bmi_op_id)
             break;
         case NA_CB_RECV_EXPECTED:
             /* Check buf_size and actual_size */
-            if (na_bmi_op_id->info.recv_expected.actual_size !=
+            if (na_bmi_op_id->info.recv_expected.actual_size >
                     na_bmi_op_id->info.recv_expected.buf_size) {
-                NA_LOG_ERROR("Buffer size and actual transfer size do not match");
+                NA_LOG_ERROR("Expected recv size too large for buffer");
                 ret = NA_SIZE_ERROR;
                 goto done;
             }
