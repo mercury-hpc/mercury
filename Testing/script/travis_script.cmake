655c5e263d9c39299b6068130f6cc080ac8e5e14
Jerome Soumagne <jsoumagne@hdfgroup.org>
2016-05-19 16:36:52 -0500

    Try to pass ltsan again to travis

--- Testing/script/travis_script.cmake
+++ Testing/script/travis_script.cmake
@@ -82,7 +82,7 @@ if(MERCURY_DO_MEMCHECK OR MERCURY_MEMORYCHECK_TYPE)
   endif()
   if(${MERCURY_MEMORYCHECK_TYPE} MATCHES "ThreadSanitizer")
     set(MERCURY_MEMCHECK_FLAGS "-O1 -fsanitize=thread -fno-omit-frame-pointer -fPIC -pie")
-    set(ENV{LDFLAGS} "-L/usr/lib/x86_64-linux-gnu -ltsan")
+    set(ENV{LDFLAGS} "/usr/lib/x86_64-linux-gnu/libtsan.so.0")
   endif()
 
   # needed by mercury_common.cmake
