a8a60318c5a2be27dcd5bf51524e832bd48cc271
Scott Atchley <atchleyes@ornl.gov>
2016-06-02 00:42:49 -0500

    Add blocking support to na_cci (fixes #72)

--- src/na/na_cci.c
+++ src/na/na_cci.c
@@ -24,6 +24,7 @@
 #include <inttypes.h>
 #include <sys/queue.h>
 #include <assert.h>
+#include <poll.h>
 
 /****************/
 /* Local Macros */
@@ -152,6 +153,7 @@ struct na_cci_private_data {
     hg_list_t *accept_conn_list; /* List of accepted connections */
     hg_thread_mutex_t accept_conn_list_mutex; /* Mutex */
     char *uri;
+    int fd;
 };
 
 typedef union cci_msg {
@@ -443,6 +445,7 @@ na_cci_initialize(na_class_t * na_class, const struct na_info *na_info,
     char *uri = NULL;
     na_return_t ret = NA_SUCCESS;
     char *string_port = NULL;
+    int fd = 0;
 
     /* Initialize CCI */
     rc = cci_init(CCI_ABI_VERSION, 0, &caps);
@@ -486,7 +489,7 @@ na_cci_initialize(na_class_t * na_class, const struct na_info *na_info,
 
     /* Create an endpoint using the requested transport */
     if(!listen) {
-       rc = cci_create_endpoint(device, 0, &endpoint, NULL);
+       rc = cci_create_endpoint(device, 0, &endpoint, &fd);
     } else {
         /* In listen mode we honor the port description */
         string_port = rindex(na_info->port_name, ':');
@@ -496,7 +499,7 @@ na_cci_initialize(na_class_t * na_class, const struct na_info *na_info,
            goto out;
         }
         string_port++;
-        rc = cci_create_endpoint_at(device, string_port, 0, &endpoint, NULL);
+        rc = cci_create_endpoint_at(device, string_port, 0, &endpoint, &fd);
     }
     if (rc) {
         NA_LOG_ERROR("cci_create_endpoint() failed with %s",
@@ -505,6 +508,7 @@ na_cci_initialize(na_class_t * na_class, const struct na_info *na_info,
         goto out;
     }
     NA_CCI_PRIVATE_DATA(na_class)->endpoint = endpoint;
+    NA_CCI_PRIVATE_DATA(na_class)->fd = fd;
 
     rc = cci_get_opt(endpoint, CCI_OPT_ENDPT_URI, &uri);
     if (rc) {
@@ -1881,8 +1885,14 @@ na_cci_progress(na_class_t * na_class, na_context_t * context,
         int rc;
         hg_time_t t1, t2;
         cci_event_t *event = NULL;
+        struct pollfd pfd;
 
         hg_time_get_current(&t1);
+
+        pfd.fd = NA_CCI_PRIVATE_DATA(na_class)->fd;
+        pfd.events = POLLIN;
+
+        poll(&pfd, 1, (int)(remaining * 1000.0));
 
         rc = cci_get_event(e, &event);
         if (rc) {
@@ -1892,6 +1902,8 @@ na_cci_progress(na_class_t * na_class, na_context_t * context,
 
             hg_time_get_current(&t2);
             remaining -= hg_time_to_double(hg_time_subtract(t2, t1));
+            if (remaining < 0)
+                break; /* Return NA_TIMEOUT */
             continue;
         }
 
