8de04b8479486fde68a7d5cec211a3fb1bb6f8a9
Jerome Soumagne <jsoumagne@hdfgroup.org>
2016-11-21 17:52:08 -0600

    Add OSX builds

--- Testing/script/travis_build.sh
+++ Testing/script/travis_build.sh
@@ -35,10 +35,10 @@ if [ ! -d "$HOME/install/bin" ]; then
       tar -xjf gcc-${GCC_VERSION}.tar.bz2;
       cd gcc-${GCC_VERSION} && ./configure --disable-bootstrap --enable-languages=c,c++ --prefix=$HOME/install --enable-shared --enable-threads=posix --enable-checking=release --with-system-zlib --enable-linker-build-id --with-linker-hash-style=gnu --enable-initfini-array --disable-libgcj --without-isl --enable-gnu-indirect-function --with-tune=generic --disable-multilib --with-gmp=$HOME/install --with-mpc=$HOME/install --with-mpfr=$HOME/install && make -j2 -s && make -s install;
     fi
-  else # OSX
-    # CMake
-    cd $HOME && wget --no-check-certificate http://www.cmake.org/files/v${CMAKE_VERSION_MAJOR}/cmake-${CMAKE_VERSION_MAJOR}.${CMAKE_VERSION_MINOR}-Darwin-x86_64.tar.gz;
-    tar --strip-components=3 -xzC $HOME/install -f cmake-${CMAKE_VERSION_MAJOR}.${CMAKE_VERSION_MINOR}-Darwin-x86_64.tar.gz;
+    # MPI
+    cd $HOME && wget http://www.mpich.org/static/downloads/${MPI_VERSION}/mpich-${MPI_VERSION}.tar.gz;
+    tar -xzf mpich-${MPI_VERSION}.tar.gz;
+    cd mpich-${MPI_VERSION} && ./configure --disable-fortran --prefix=$HOME/install && make -j2 -s && make install;
   fi
   # BMI
   cd $HOME && git clone git://git.mcs.anl.gov/bmi bmi && cd bmi;
@@ -46,10 +46,6 @@ if [ ! -d "$HOME/install/bin" ]; then
       patch -p1 < ${TRAVIS_BUILD_DIR}/Testing/script/bmi_osx.patch
   fi
   ./prepare && ./configure --enable-shared --enable-bmi-only --prefix=$HOME/install && make -j2 -s && make install;
-  # MPI
-  cd $HOME && wget http://www.mpich.org/static/downloads/${MPI_VERSION}/mpich-${MPI_VERSION}.tar.gz;
-  tar -xzf mpich-${MPI_VERSION}.tar.gz;
-  cd mpich-${MPI_VERSION} && ./configure --disable-fortran --prefix=$HOME/install && make -j2 -s && make install;
   # CCI
   cd $HOME && wget http://cci-forum.com/wp-content/uploads/2016/06/cci-${CCI_VERSION}.tar.gz
   tar -xzf cci-${CCI_VERSION}.tar.gz && cd cci-${CCI_VERSION};
