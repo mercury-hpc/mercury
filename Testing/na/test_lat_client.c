b4b2d95877b679e9df389b7b8e6d50434ac19fdb
Jerome Soumagne <jsoumagne@hdfgroup.org>
2017-12-18 17:06:10 -0600

    Add NA latency test

---
+++ Testing/na/test_lat_client.c
@@ -0,0 +1,419 @@
+/*
+ * Copyright (C) 2013-2017 Argonne National Laboratory, Department of Energy,
+ *                    UChicago Argonne, LLC and The HDF Group.
+ * All rights reserved.
+ *
+ * The full copyright notice, including terms governing use, modification,
+ * and redistribution, is contained in the COPYING file that can be
+ * found at the root of the source code distribution tree.
+ */
+
+#include "na_test.h"
+
+#include "mercury_request.h" /* For convenience */
+#include "mercury_time.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+/****************/
+/* Local Macros */
+/****************/
+#define BENCHMARK_NAME "Message latency"
+#define STRING(s) #s
+#define XSTRING(s) STRING(s)
+#define VERSION_NAME \
+    XSTRING(0) \
+    "." \
+    XSTRING(1) \
+    "." \
+    XSTRING(0)
+
+#define SMALL_SKIP          1000
+
+#define NDIGITS             2
+#define NWIDTH              20
+#define NA_TEST_TAG_DONE    111
+
+/************************************/
+/* Local Type and Struct Definition */
+/************************************/
+
+struct na_test_lat_info {
+    na_class_t *na_class;
+    na_context_t *context;
+    hg_request_class_t *request_class;
+    na_addr_t target_addr;
+    struct na_test_info na_test_info;
+};
+
+struct na_test_target_lookup_arg {
+    na_addr_t *addr_ptr;
+    hg_request_t *request;
+};
+
+/********************/
+/* Local Prototypes */
+/********************/
+
+static NA_INLINE int
+na_test_request_progress(unsigned int timeout, void *arg);
+
+static NA_INLINE int
+na_test_request_trigger(unsigned int timeout, unsigned int *flag, void *arg);
+
+static na_return_t
+na_test_target_lookup(struct na_test_lat_info *na_test_lat_info);
+
+static NA_INLINE int
+na_test_target_lookup_cb(const struct na_cb_info *na_cb_info);
+
+static NA_INLINE int
+na_test_recv_expected_cb(const struct na_cb_info *na_cb_info);
+
+static na_return_t
+na_test_measure_latency(struct na_test_lat_info *na_test_lat_info,
+    na_size_t size);
+
+/*******************/
+/* Local Variables */
+/*******************/
+
+/*---------------------------------------------------------------------------*/
+static NA_INLINE int
+na_test_request_progress(unsigned int timeout, void *arg)
+{
+    struct na_test_lat_info *na_test_lat_info = (struct na_test_lat_info *) arg;
+    unsigned int timeout_progress = 0;
+    int ret = HG_UTIL_SUCCESS;
+
+    /* Safe to block */
+    if (NA_Poll_try_wait(na_test_lat_info->na_class, na_test_lat_info->context))
+        timeout_progress = timeout;
+
+    /* Progress */
+    if (NA_Progress(na_test_lat_info->na_class, na_test_lat_info->context,
+        timeout_progress) != NA_SUCCESS)
+        ret = HG_UTIL_FAIL;
+
+    return ret;
+}
+
+/*---------------------------------------------------------------------------*/
+static NA_INLINE int
+na_test_request_trigger(unsigned int timeout, unsigned int *flag, void *arg)
+{
+    struct na_test_lat_info *na_test_lat_info = (struct na_test_lat_info *) arg;
+    unsigned int actual_count = 0;
+    int ret = HG_UTIL_SUCCESS;
+
+    if (NA_Trigger(na_test_lat_info->context, timeout, 1, NULL, &actual_count)
+        != NA_SUCCESS) ret = HG_UTIL_FAIL;
+    *flag = (actual_count) ? HG_UTIL_TRUE : HG_UTIL_FALSE;
+
+    return ret;
+}
+
+/*---------------------------------------------------------------------------*/
+static na_return_t
+na_test_target_lookup(struct na_test_lat_info *na_test_lat_info)
+{
+    struct na_test_target_lookup_arg request_args = { 0 };
+    hg_request_t *request = NULL;
+    na_return_t ret = NA_SUCCESS;
+
+    request = hg_request_create(na_test_lat_info->request_class);
+    request_args.addr_ptr = &na_test_lat_info->target_addr;
+    request_args.request = request;
+
+    /* Forward call to remote addr and get a new request */
+    ret = NA_Addr_lookup(na_test_lat_info->na_class, na_test_lat_info->context,
+        na_test_target_lookup_cb, &request_args,
+        na_test_lat_info->na_test_info.target_name, NA_OP_ID_IGNORE);
+    if (ret != NA_SUCCESS) {
+        NA_LOG_ERROR("Could not lookup address");
+        goto done;
+    }
+
+    /* Wait for request to be marked completed */
+    hg_request_wait(request, NA_MAX_IDLE_TIME, NULL);
+
+done:
+    hg_request_destroy(request);
+    return ret;
+}
+
+/*---------------------------------------------------------------------------*/
+static NA_INLINE int
+na_test_target_lookup_cb(const struct na_cb_info *na_cb_info)
+{
+    struct na_test_target_lookup_arg *na_test_target_lookup_arg =
+        (struct na_test_target_lookup_arg *) na_cb_info->arg;
+
+    *na_test_target_lookup_arg->addr_ptr = na_cb_info->info.lookup.addr;
+
+    hg_request_complete(na_test_target_lookup_arg->request);
+
+    return NA_SUCCESS;
+}
+
+/*---------------------------------------------------------------------------*/
+static NA_INLINE int
+na_test_recv_expected_cb(const struct na_cb_info *na_cb_info)
+{
+    hg_request_t *request = (hg_request_t *) na_cb_info->arg;
+
+    hg_request_complete(request);
+
+    return NA_SUCCESS;
+}
+
+/*---------------------------------------------------------------------------*/
+static na_return_t
+na_test_measure_latency(struct na_test_lat_info *na_test_lat_info,
+    na_size_t size)
+{
+    char *send_buf = NULL, *recv_buf = NULL;
+    void *send_buf_data, *recv_buf_data;
+    size_t loop = MERCURY_TESTING_MAX_LOOP * 100;
+    size_t skip = SMALL_SKIP;
+    na_op_id_t send_op_id;
+    na_op_id_t recv_op_id;
+    hg_request_t *recv_request = NULL;
+    na_size_t unexpected_header_size =
+        NA_Msg_get_unexpected_header_size(na_test_lat_info->na_class);
+    na_size_t buf_size =
+        size < unexpected_header_size ? unexpected_header_size : size;
+    size_t avg_iter;
+    double time_read = 0, read_lat;
+    na_return_t ret = NA_SUCCESS;
+    size_t i;
+
+    /* Prepare send_buf */
+    if (buf_size == unexpected_header_size)
+        buf_size++;
+    send_buf = NA_Msg_buf_alloc(na_test_lat_info->na_class, buf_size,
+        &send_buf_data);
+    NA_Msg_init_unexpected(na_test_lat_info->na_class, send_buf, buf_size);
+    for (i = unexpected_header_size; i < buf_size; i++)
+        send_buf[i] = (char) i;
+
+    /* Prepare recv buf */
+    recv_buf = NA_Msg_buf_alloc(na_test_lat_info->na_class, buf_size,
+        &recv_buf_data);
+    memset(recv_buf, 0, buf_size);
+
+    /* Create operation IDs */
+    send_op_id = NA_Op_create(na_test_lat_info->na_class);
+    recv_op_id = NA_Op_create(na_test_lat_info->na_class);
+
+    recv_request = hg_request_create(na_test_lat_info->request_class);
+
+    /* Warm up */
+    for (i = 0; i < skip; i++) {
+        /* Post recv */
+        ret = NA_Msg_recv_expected(na_test_lat_info->na_class,
+            na_test_lat_info->context, na_test_recv_expected_cb, recv_request,
+            recv_buf, buf_size, recv_buf_data, na_test_lat_info->target_addr, 0,
+            &recv_op_id);
+        if (ret != NA_SUCCESS) {
+            NA_LOG_ERROR("NA_Msg_recv_expected() failed");
+            goto done;
+        }
+
+        /* Post send */
+        ret = NA_Msg_send_unexpected(na_test_lat_info->na_class,
+            na_test_lat_info->context, NULL, NULL, send_buf, buf_size,
+            send_buf_data, na_test_lat_info->target_addr, 0, &send_op_id);
+        if (ret != NA_SUCCESS) {
+            NA_LOG_ERROR("NA_Msg_send_unexpected() failed");
+            goto done;
+        }
+
+        hg_request_wait(recv_request, NA_MAX_IDLE_TIME, NULL);
+        hg_request_reset(recv_request);
+    }
+
+    NA_Test_barrier(&na_test_lat_info->na_test_info);
+
+    /* Actual benchmark */
+    for (avg_iter = 0; avg_iter < loop; avg_iter++) {
+        hg_time_t t1, t2;
+
+        hg_time_get_current(&t1);
+
+        /* Post recv */
+        ret = NA_Msg_recv_expected(na_test_lat_info->na_class,
+            na_test_lat_info->context, na_test_recv_expected_cb, recv_request,
+            recv_buf, buf_size, recv_buf_data, na_test_lat_info->target_addr,
+            1, &recv_op_id);
+        if (ret != NA_SUCCESS) {
+            NA_LOG_ERROR("NA_Msg_recv_expected() failed");
+            goto done;
+        }
+
+        /* Post send */
+        ret = NA_Msg_send_unexpected(na_test_lat_info->na_class,
+            na_test_lat_info->context, NULL, NULL, send_buf, buf_size,
+            send_buf_data, na_test_lat_info->target_addr, 1,
+            &send_op_id);
+        if (ret != NA_SUCCESS) {
+            NA_LOG_ERROR("NA_Msg_send_unexpected() failed");
+            goto done;
+        }
+
+        hg_request_wait(recv_request, NA_MAX_IDLE_TIME, NULL);
+        NA_Test_barrier(&na_test_lat_info->na_test_info);
+        hg_time_get_current(&t2);
+        time_read += hg_time_to_double(hg_time_subtract(t2, t1));
+
+        hg_request_reset(recv_request);
+
+#ifdef MERCURY_TESTING_HAS_VERIFY_DATA
+    /* Check recv buf */
+    char *recv_buf_ptr = (const char*) recv_buf;
+
+    for (i = NA_Msg_get_unexpected_header_size(na_test_lat_info->na_class);
+        i < buf_size; i++) {
+        if (recv_buf_ptr[i] != (char) i) {
+            fprintf(stderr, "Error detected in bulk transfer, buf[%d] = %d, "
+                "was expecting %d!\n", (int) i, (char) recv_buf_ptr[i],
+                (char) i);
+            break;
+        }
+    }
+#endif
+
+    /* At this point we have received everything so work out the bandwidth */
+#ifdef MERCURY_TESTING_PRINT_PARTIAL
+        read_lat = time_read * 1.0e6
+            / (double) ((avg_iter + 1) * 2 *
+                (unsigned int) na_test_lat_info->na_test_info.mpi_comm_size);
+        if (na_test_lat_info->na_test_info.mpi_comm_rank == 0)
+            fprintf(stdout, "%-*d%*.*f\r", 10, (int) size, NWIDTH,
+                NDIGITS, read_lat);
+#endif
+    }
+#ifndef MERCURY_TESTING_PRINT_PARTIAL
+    read_lat = time_read * 1.0e6
+        / (double) (loop * 2 *
+            (unsigned int) na_test_lat_info->na_test_info.mpi_comm_size);
+    if (na_test_lat_info->na_test_info.mpi_comm_rank == 0)
+        fprintf(stdout, "%-*d%*.*f", 10, (int) size, NWIDTH, NDIGITS,
+            read_lat);
+#endif
+    if (na_test_lat_info->na_test_info.mpi_comm_rank == 0)
+        fprintf(stdout, "\n");
+
+done:
+    /* Clean up resources */
+    hg_request_destroy(recv_request);
+    NA_Op_destroy(na_test_lat_info->na_class, send_op_id);
+    NA_Op_destroy(na_test_lat_info->na_class, recv_op_id);
+    NA_Msg_buf_free(na_test_lat_info->na_class, send_buf, send_buf_data);
+    NA_Msg_buf_free(na_test_lat_info->na_class, recv_buf, recv_buf_data);
+    return ret;
+}
+
+/*---------------------------------------------------------------------------*/
+static na_return_t
+na_test_send_finalize(struct na_test_lat_info *na_test_lat_info)
+{
+    char *send_buf = NULL, *recv_buf = NULL;
+    void *send_buf_data, *recv_buf_data;
+    hg_request_t *recv_request = NULL;
+    na_size_t unexpected_header_size =
+        NA_Msg_get_unexpected_header_size(na_test_lat_info->na_class);
+    na_size_t buf_size =
+        (unexpected_header_size) ? unexpected_header_size + 1 : 1;
+    na_return_t ret = NA_SUCCESS;
+
+    /* Prepare send_buf */
+    send_buf = NA_Msg_buf_alloc(na_test_lat_info->na_class, buf_size,
+        &send_buf_data);
+    NA_Msg_init_unexpected(na_test_lat_info->na_class, send_buf, buf_size);
+
+    /* Prepare recv buf */
+    recv_buf = NA_Msg_buf_alloc(na_test_lat_info->na_class, buf_size,
+        &recv_buf_data);
+    memset(recv_buf, 0, buf_size);
+
+    recv_request = hg_request_create(na_test_lat_info->request_class);
+
+    /* Post recv */
+    ret = NA_Msg_recv_expected(na_test_lat_info->na_class,
+        na_test_lat_info->context, na_test_recv_expected_cb, recv_request,
+        recv_buf, buf_size, recv_buf_data, na_test_lat_info->target_addr,
+        NA_TEST_TAG_DONE, NA_OP_ID_IGNORE);
+    if (ret != NA_SUCCESS) {
+        NA_LOG_ERROR("NA_Msg_recv_expected() failed");
+        goto done;
+    }
+
+    /* Post send */
+    ret = NA_Msg_send_unexpected(na_test_lat_info->na_class,
+        na_test_lat_info->context, NULL, NULL, send_buf, buf_size,
+        send_buf_data, na_test_lat_info->target_addr, NA_TEST_TAG_DONE,
+        NA_OP_ID_IGNORE);
+    if (ret != NA_SUCCESS) {
+        NA_LOG_ERROR("NA_Msg_send_unexpected() failed");
+        goto done;
+    }
+
+    hg_request_wait(recv_request, NA_MAX_IDLE_TIME, NULL);
+
+done:
+    /* Clean up resources */
+    hg_request_destroy(recv_request);
+    NA_Msg_buf_free(na_test_lat_info->na_class, send_buf, send_buf_data);
+    NA_Msg_buf_free(na_test_lat_info->na_class, recv_buf, recv_buf_data);
+    return ret;
+}
+
+/*---------------------------------------------------------------------------*/
+int
+main(int argc, char *argv[])
+{
+    struct na_test_lat_info na_test_lat_info = { 0 };
+    na_size_t size, max_size;
+    int ret = EXIT_SUCCESS;
+
+    /* Initialize the interface */
+    NA_Test_init(argc, argv, &na_test_lat_info.na_test_info);
+    na_test_lat_info.na_class = na_test_lat_info.na_test_info.na_class;
+    na_test_lat_info.context = NA_Context_create(na_test_lat_info.na_class);
+    na_test_lat_info.request_class = hg_request_init(na_test_request_progress,
+        na_test_request_trigger, &na_test_lat_info);
+
+    /* Lookup target addr */
+    na_test_target_lookup(&na_test_lat_info);
+
+    /* Set max size */
+    max_size = NA_Msg_get_max_unexpected_size(na_test_lat_info.na_class);
+
+    if (na_test_lat_info.na_test_info.mpi_comm_rank == 0) {
+        fprintf(stdout, "# %s v%s\n", BENCHMARK_NAME, VERSION_NAME);
+        fprintf(stdout, "# Loop %d times from size %d to %zu byte(s)\n",
+            MERCURY_TESTING_MAX_LOOP, 1, max_size);
+#ifdef MERCURY_TESTING_HAS_VERIFY_DATA
+        fprintf(stdout, "# WARNING verifying data, output will be slower\n");
+#endif
+        fprintf(stdout, "%-*s%*s\n", 10, "# Size", NWIDTH,
+            "Latency (us)");
+        fflush(stdout);
+    }
+
+    /* Msg with different sizes */
+    for (size = 1; size <= max_size; size *= 2)
+        na_test_measure_latency(&na_test_lat_info, size);
+
+    /* Finalize interface */
+    if (na_test_lat_info.na_test_info.mpi_comm_rank == 0)
+        na_test_send_finalize(&na_test_lat_info);
+    NA_Addr_free(na_test_lat_info.na_class, na_test_lat_info.target_addr);
+    hg_request_finalize(na_test_lat_info.request_class, NULL);
+    NA_Context_destroy(na_test_lat_info.na_class, na_test_lat_info.context);
+    NA_Test_finalize(&na_test_lat_info.na_test_info);
+
+    return ret;
+}
---
+++ Testing/na/test_lat_server.c
@@ -0,0 +1,255 @@
+/*
+ * Copyright (C) 2013-2017 Argonne National Laboratory, Department of Energy,
+ *                    UChicago Argonne, LLC and The HDF Group.
+ * All rights reserved.
+ *
+ * The full copyright notice, including terms governing use, modification,
+ * and redistribution, is contained in the COPYING file that can be
+ * found at the root of the source code distribution tree.
+ */
+
+#include "na_test.h"
+
+#include "mercury_request.h" /* For convenience */
+#include "mercury_time.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+/****************/
+/* Local Macros */
+/****************/
+
+#define NA_TEST_TAG_DONE 111
+
+/************************************/
+/* Local Type and Struct Definition */
+/************************************/
+
+struct na_test_lat_info {
+    na_class_t *na_class;
+    na_context_t *context;
+    hg_request_class_t *request_class;
+    struct na_test_info na_test_info;
+};
+
+struct na_test_source_recv_arg {
+    void *recv_buf;
+    void *send_buf;
+    void *send_buf_data;
+    na_tag_t tag;
+    na_op_id_t send_op_id;
+    hg_request_t *request;
+    struct na_test_lat_info *na_test_lat_info;
+};
+
+/********************/
+/* Local Prototypes */
+/********************/
+
+static NA_INLINE int
+na_test_request_progress(unsigned int timeout, void *arg);
+
+static NA_INLINE int
+na_test_request_trigger(unsigned int timeout, unsigned int *flag, void *arg);
+
+static NA_INLINE int
+na_test_recv_unexpected_cb(const struct na_cb_info *na_cb_info);
+
+static NA_INLINE int
+na_test_send_expected_cb(const struct na_cb_info *na_cb_info);
+
+static na_return_t
+na_test_loop_latency(struct na_test_lat_info *na_test_lat_info);
+
+/*******************/
+/* Local Variables */
+/*******************/
+
+/*---------------------------------------------------------------------------*/
+static NA_INLINE int
+na_test_request_progress(unsigned int timeout, void *arg)
+{
+    struct na_test_lat_info *na_test_lat_info = (struct na_test_lat_info *) arg;
+    unsigned int timeout_progress = 0;
+    int ret = HG_UTIL_SUCCESS;
+
+    /* Safe to block */
+    if (NA_Poll_try_wait(na_test_lat_info->na_class, na_test_lat_info->context))
+        timeout_progress = timeout;
+
+    /* Progress */
+    if (NA_Progress(na_test_lat_info->na_class, na_test_lat_info->context,
+        timeout_progress) != NA_SUCCESS)
+        ret = HG_UTIL_FAIL;
+
+    return ret;
+}
+
+/*---------------------------------------------------------------------------*/
+static NA_INLINE int
+na_test_request_trigger(unsigned int timeout, unsigned int *flag, void *arg)
+{
+    struct na_test_lat_info *na_test_lat_info = (struct na_test_lat_info *) arg;
+    unsigned int actual_count = 0;
+    int ret = HG_UTIL_SUCCESS;
+
+    if (NA_Trigger(na_test_lat_info->context, timeout, 1, NULL, &actual_count)
+        != NA_SUCCESS) ret = HG_UTIL_FAIL;
+    *flag = (actual_count) ? HG_UTIL_TRUE : HG_UTIL_FALSE;
+
+    return ret;
+}
+
+/*---------------------------------------------------------------------------*/
+static NA_INLINE int
+na_test_recv_unexpected_cb(const struct na_cb_info *na_cb_info)
+{
+    struct na_test_source_recv_arg *na_test_source_recv_arg =
+        (struct na_test_source_recv_arg *) na_cb_info->arg;
+    struct na_test_lat_info *na_test_lat_info =
+        na_test_source_recv_arg->na_test_lat_info;
+    na_return_t ret;
+
+    na_test_source_recv_arg->tag = na_cb_info->info.recv_unexpected.tag;
+#ifdef MERCURY_TESTING_HAS_VERIFY_DATA
+    if (na_test_source_recv_arg->tag != NA_TEST_TAG_DONE) {
+        /* Check recv buf */
+        char *recv_buf_ptr = (const char*) na_test_source_recv_arg->recv_buf;
+        na_size_t i;
+
+        for (i = NA_Msg_get_unexpected_header_size(na_test_lat_info->na_class);
+            i < na_cb_info->info.recv_unexpected.actual_buf_size; i++) {
+            if (recv_buf_ptr[i] != (char) i) {
+                fprintf(stderr, "Error detected in bulk transfer, buf[%d] = %d, "
+                    "was expecting %d!\n", (int) i, (char) recv_buf_ptr[i],
+                    (char) i);
+                break;
+            }
+        }
+    }
+#endif
+
+    /* Post send */
+    ret = NA_Msg_send_expected(na_test_lat_info->na_class,
+        na_test_lat_info->context, na_test_send_expected_cb,
+        na_test_source_recv_arg->request,
+        na_test_source_recv_arg->send_buf,
+        na_cb_info->info.recv_unexpected.actual_buf_size,
+        na_test_source_recv_arg->send_buf_data,
+        na_cb_info->info.recv_unexpected.source,
+        na_cb_info->info.recv_unexpected.tag,
+        &na_test_source_recv_arg->send_op_id);
+    if (ret != NA_SUCCESS) {
+        NA_LOG_ERROR("NA_Msg_send_expected() failed");
+    }
+
+    NA_Addr_free(na_test_lat_info->na_class,
+        na_cb_info->info.recv_unexpected.source);
+    return NA_SUCCESS;
+}
+
+/*---------------------------------------------------------------------------*/
+static NA_INLINE int
+na_test_send_expected_cb(const struct na_cb_info *na_cb_info)
+{
+    hg_request_t *request = (hg_request_t *) na_cb_info->arg;
+
+    hg_request_complete(request);
+
+    return NA_SUCCESS;
+}
+
+/*---------------------------------------------------------------------------*/
+static na_return_t
+na_test_loop_latency(struct na_test_lat_info *na_test_lat_info)
+{
+    char *send_buf = NULL, *recv_buf = NULL;
+    void *send_buf_data, *recv_buf_data;
+    na_op_id_t send_op_id;
+    na_op_id_t recv_op_id;
+    hg_request_t *send_request = NULL;
+    na_size_t unexpected_size =
+        NA_Msg_get_max_unexpected_size(na_test_lat_info->na_class);
+    na_size_t expected_size =
+        NA_Msg_get_max_expected_size(na_test_lat_info->na_class);
+    na_size_t i;
+    na_return_t ret = NA_SUCCESS;
+
+    /* Prepare send_buf */
+    send_buf = NA_Msg_buf_alloc(na_test_lat_info->na_class, expected_size,
+        &send_buf_data);
+    for (i = 0; i < expected_size; i++)
+        send_buf[i] = (char) i;
+
+    /* Prepare recv buf */
+    recv_buf = NA_Msg_buf_alloc(na_test_lat_info->na_class, unexpected_size,
+        &recv_buf_data);
+    memset(recv_buf, 0, unexpected_size);
+
+    /* Create operation IDs */
+    send_op_id = NA_Op_create(na_test_lat_info->na_class);
+    recv_op_id = NA_Op_create(na_test_lat_info->na_class);
+
+    send_request = hg_request_create(na_test_lat_info->request_class);
+
+    struct na_test_source_recv_arg na_test_source_recv_arg = { 0 };
+    na_test_source_recv_arg.request = send_request;
+    na_test_source_recv_arg.recv_buf = recv_buf;
+    na_test_source_recv_arg.send_buf = send_buf;
+    na_test_source_recv_arg.send_buf_data = send_buf_data;
+    na_test_source_recv_arg.send_op_id = send_op_id;
+    na_test_source_recv_arg.na_test_lat_info = na_test_lat_info;
+
+    while (na_test_source_recv_arg.tag != NA_TEST_TAG_DONE) {
+        /* Post recv */
+        ret = NA_Msg_recv_unexpected(na_test_lat_info->na_class,
+            na_test_lat_info->context, na_test_recv_unexpected_cb,
+            &na_test_source_recv_arg, recv_buf, unexpected_size,
+            recv_buf_data, 0, &recv_op_id);
+        if (ret != NA_SUCCESS) {
+            NA_LOG_ERROR("NA_Msg_recv_unexpected() failed");
+            goto done;
+        }
+
+        hg_request_wait(send_request, NA_MAX_IDLE_TIME, NULL);
+        hg_request_reset(send_request);
+    }
+
+done:
+    /* Clean up resources */
+    hg_request_destroy(send_request);
+    NA_Op_destroy(na_test_lat_info->na_class, send_op_id);
+    NA_Op_destroy(na_test_lat_info->na_class, recv_op_id);
+    NA_Msg_buf_free(na_test_lat_info->na_class, send_buf, send_buf_data);
+    NA_Msg_buf_free(na_test_lat_info->na_class, recv_buf, recv_buf_data);
+    return ret;
+}
+
+/*---------------------------------------------------------------------------*/
+int
+main(int argc, char *argv[])
+{
+    struct na_test_lat_info na_test_lat_info = { 0 };
+    int ret = EXIT_SUCCESS;
+
+    /* Initialize the interface */
+    na_test_lat_info.na_test_info.listen = NA_TRUE;
+    NA_Test_init(argc, argv, &na_test_lat_info.na_test_info);
+    na_test_lat_info.na_class = na_test_lat_info.na_test_info.na_class;
+    na_test_lat_info.context = NA_Context_create(na_test_lat_info.na_class);
+    na_test_lat_info.request_class = hg_request_init(na_test_request_progress,
+        na_test_request_trigger, &na_test_lat_info);
+
+    /* Process */
+    na_test_loop_latency(&na_test_lat_info);
+
+    printf("Finalizing...\n");
+
+    /* Finalize interface */
+    hg_request_finalize(na_test_lat_info.request_class, NULL);
+    NA_Context_destroy(na_test_lat_info.na_class, na_test_lat_info.context);
+    NA_Test_finalize(&na_test_lat_info.na_test_info);
+
+    return ret;
+}
