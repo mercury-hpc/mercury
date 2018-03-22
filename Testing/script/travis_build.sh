a0038b2fedf211c2b7044630287d9755073ea56a
Jerome Soumagne <jsoumagne@hdfgroup.org>
2018-03-22 16:28:54 -0500

    Update travis build script to use latest package versions

--- Testing/script/travis_build.sh
+++ Testing/script/travis_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CMAKE_VERSION_MAJOR=3.10
-CMAKE_VERSION_MINOR=1
-MPI_VERSION=3.3a2
+CMAKE_VERSION_MINOR=3
+MPI_VERSION=3.3b1
 CCI_VERSION=2.1
-OFI_VERSION=1.5.3
+OFI_VERSION=1.6.0
 PREFIX=$HOME/install
 
 set -e
@@ -22,18 +22,26 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
   fi
 
   # CMake
-  if [ ! -f "$PREFIX/bin/cmake" ]; then
+  if [ -f "$PREFIX/cmake_version.txt" ]; then
+    CMAKE_INSTALLED_VERSION=`cat $PREFIX/cmake_version.txt`;
+  fi
+  if [ ! -f "$PREFIX/bin/cmake" ] || [ "$CMAKE_INSTALLED_VERSION" != "${CMAKE_VERSION_MAJOR}.${CMAKE_VERSION_MINOR}" ]; then
     cd $HOME && wget --no-check-certificate http://www.cmake.org/files/v${CMAKE_VERSION_MAJOR}/cmake-${CMAKE_VERSION_MAJOR}.${CMAKE_VERSION_MINOR}-Linux-x86_64.tar.gz;
     tar --strip-components=1 -xzC $PREFIX -f cmake-${CMAKE_VERSION_MAJOR}.${CMAKE_VERSION_MINOR}-Linux-x86_64.tar.gz;
+    echo "${CMAKE_VERSION_MAJOR}.${CMAKE_VERSION_MINOR}" > $PREFIX/cmake_version.txt
   else
     echo "Using cached directory for CMake";
   fi
 
   # MPI
-  if [ ! -f "$PREFIX/bin/mpicc" ]; then
+  if [ -f "$PREFIX/mpi_version.txt" ]; then
+    MPI_INSTALLED_VERSION=`cat $PREFIX/mpi_version.txt`;
+  fi
+  if [ ! -f "$PREFIX/bin/mpicc" ] || [ "${MPI_INSTALLED_VERSION}" != "${MPI_VERSION}" ]; then
     cd $HOME && wget http://www.mpich.org/static/downloads/${MPI_VERSION}/mpich-${MPI_VERSION}.tar.gz;
     tar -xzf mpich-${MPI_VERSION}.tar.gz;
     cd mpich-${MPI_VERSION} && ./configure --disable-fortran --prefix=$PREFIX && make -j2 -s && make install;
+    echo "${MPI_VERSION}" > $PREFIX/mpi_version.txt
   else
     echo "Using cached directory for MPI";
   fi
@@ -49,11 +57,16 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
   fi
 
   # OFI
-  if [ ! -f "$PREFIX/bin/fi_info" ]; then
+  if [ -f "$PREFIX/ofi_version.txt" ]; then
+    OFI_INSTALLED_VERSION=`cat $PREFIX/ofi_version.txt`;
+  fi
+  if [ ! -f "$PREFIX/bin/fi_info" ] || [ "${OFI_INSTALLED_VERSION}" != "${OFI_VERSION}" ]; then
     cd $HOME && wget https://github.com/ofiwg/libfabric/releases/download/v${OFI_VERSION}/libfabric-${OFI_VERSION}.tar.bz2
     tar -xjf libfabric-${OFI_VERSION}.tar.bz2;
     cd libfabric-${OFI_VERSION} && ./configure --prefix=$PREFIX --disable-rxd --disable-rxm --disable-usnic --disable-static --disable-silent-rules CFLAGS="-O2 -g" && make -j2 -s && make install;
+    echo "${OFI_VERSION}" > $PREFIX/ofi_version.txt
   else
     echo "Using cached directory for OFI";
   fi
 fi
+
