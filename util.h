1ece46405442f499488ffac09e0fc5c1fef78877
David Young <dyoung@hdfgroup.org>
2020-10-12 15:49:58 -0500

    Get uint8_t and size_t definitions with the proper #includes.

--- util.h
+++ util.h
@@ -1,6 +1,9 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+#include <stdint.h> /* uint8_t */
+#include <unistd.h> /* size_t */
+
 #define NELTS(_a)   (sizeof(_a) / sizeof((_a)[0]))
 
 int colon_separated_octets_to_bytes(const char *, uint8_t **, size_t *);
