4577943892bde2995d3f82a9f6eb08368eb2813d
David Young <dyoung@hdfgroup.org>
2020-11-10 13:59:23 -0600

    Add some comments.  Name some anonymous numbers.

--- tag.h
+++ tag.h
@@ -3,17 +3,29 @@
 
 #include "bits.h"
 
+/* Bits 63:62 indicate the message channel.  There are two unused
+ * channels that wireup ignores as of this writing but reserves for
+ * itself, a user channel that is for the use of the wireup-using
+ * application, and a wireup channel reserved for carrying wireup
+ * messages.
+ */
 #define TAG_CHNL_MASK       BITS(63, 62)
+
+/* In a message on the wireup channel, bits 61:0 carry a sender ID. */
 #define TAG_ID_MASK         BITS(61, 0)
 
-#define CHANNEL_WIREUP 1
-#define CHANNEL_TWOWAY 2
+#define CHANNEL_UNUSED0 0
+#define CHANNEL_WIREUP  1
+#define CHANNEL_USER    2
+#define CHANNEL_UNUSED3 3
 
-#define TAG_CHNL_UNUSED0    SHIFTIN(0, TAG_CHNL_MASK)
+/* Definitions suitable for bitwise-OR'ing into a tag number. */
+#define TAG_CHNL_UNUSED0    SHIFTIN(CHANNEL_UNUSED0, TAG_CHNL_MASK)
 #define TAG_CHNL_WIREUP     SHIFTIN(CHANNEL_WIREUP, TAG_CHNL_MASK)
-#define TAG_CHNL_TWOWAY     SHIFTIN(CHANNEL_TWOWAY, TAG_CHNL_MASK)
-#define TAG_CHNL_UNUSED3    SHIFTIN(3, TAG_CHNL_MASK)
+#define TAG_CHNL_USER       SHIFTIN(CHANNEL_USER, TAG_CHNL_MASK)
+#define TAG_CHNL_UNUSED3    SHIFTIN(CHANNEL_UNUSED3, TAG_CHNL_MASK)
 
+/* Extract the sender ID or the channel number from a tag number. */
 #define TAG_GET_ID(_x)      SHIFTOUT(_x, TAG_ID_MASK)
 #define TAG_GET_CHNL(_x)    SHIFTOUT(_x, TAG_CHNL_MASK)
 
