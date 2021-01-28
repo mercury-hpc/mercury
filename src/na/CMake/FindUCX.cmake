dd91223cd98f8e8387521f03b830f2d9e8be29f6
David Young <dyoung@hdfgroup.org>
2021-01-28 10:41:10 -0600

    Look for libucp, libuct, and libucs independently and add them all to the UCX libraries list.  This should stop the CI static build from failing.

--- src/na/CMake/FindUCX.cmake
+++ src/na/CMake/FindUCX.cmake
@@ -10,17 +10,24 @@ pkg_check_modules(PC_UCX QUIET ucx)
 find_path(UCX_INCLUDE_DIR ucp/api/ucp.h
   HINTS ${PC_UCX_INCLUDEDIR} ${PC_UCX_INCLUDE_DIRS})
 
-find_library(UCX_LIBRARY NAMES ucp uct ucs
+find_library(UCP_LIBRARY NAMES ucp
+  HINTS ${PC_UCX_LIBDIR} ${PC_UCX_LIBRARY_DIRS})
+
+find_library(UCT_LIBRARY NAMES uct
+  HINTS ${PC_UCX_LIBDIR} ${PC_UCX_LIBRARY_DIRS})
+
+find_library(UCS_LIBRARY NAMES ucs
   HINTS ${PC_UCX_LIBDIR} ${PC_UCX_LIBRARY_DIRS})
 
 set(UCX_INCLUDE_DIRS ${UCX_INCLUDE_DIR})
-set(UCX_LIBRARIES ${UCX_LIBRARY})
+set(UCX_LIBRARIES ${UCP_LIBRARY} ${UCT_LIBRARY} ${UCS_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set UCX_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(UCX DEFAULT_MSG
-                                  UCX_INCLUDE_DIR UCX_LIBRARY)
+                                  UCX_INCLUDE_DIR
+				  UCP_LIBRARY UCT_LIBRARY UCS_LIBRARY)
 
-mark_as_advanced(UCX_INCLUDE_DIR UCX_LIBRARY)
+mark_as_advanced(UCX_INCLUDE_DIR UCP_LIBRARY UCT_LIBRARY UCS_LIBRARY)
 
