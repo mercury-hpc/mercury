378ffdcc5ac42ac3bd47d9ec0337680fe6f462b2
David Young <dyoung@hdfgroup.org>
2021-02-05 13:23:30 -0500

    Don't use ULL on a constant, just cast it to uint64_t so that it can be formatted with PRIu64, which is not always "llu".

--- src/na/na_ofi.c
+++ src/na/na_ofi.c
@@ -173,8 +173,8 @@ static unsigned long const na_ofi_prov_flags[] = {NA_OFI_PROV_TYPES};
 
 /* Unexpected size */
 #define NA_OFI_UNEXPECTED_SIZE (4096)
-#define NA_OFI_UNEXPECTED_TAG  (0x100000000ULL)
-#define NA_OFI_TAG_MASK        (0x0FFFFFFFFULL)
+#define NA_OFI_UNEXPECTED_TAG  ((uint64_t)0x100000000)
+#define NA_OFI_TAG_MASK        ((uint64_t)0x0FFFFFFFF)
 
 /* Number of CQ event provided for fi_cq_read() */
 #define NA_OFI_CQ_EVENT_NUM (16)
