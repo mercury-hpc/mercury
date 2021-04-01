6dc5d1660e673b008997dd24e08b85d2a03e115c
Jerome Soumagne <jsoumagne@hdfgroup.org>
2021-04-01 16:18:34 -0500

    CI: turn on macos testing

--- Testing/script/gh_install_deps.sh
+++ Testing/script/gh_install_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BMI_VERSION=master
-MPI_VERSION=3.4.1
+#MPI_VERSION=3.4.1
 if [[ $MERCURY_BUILD_CONFIGURATION == 'Tsan' ]]; then
   OFI_CFLAGS="-O1 -g -fsanitize=thread"
   OFI_EXTRA_FLAGS="--enable-debug"
@@ -15,33 +15,28 @@ PREFIX=${RUNNER_TEMP}/${INSTALL_DIR}
 
 set -e
 
-if [[ ${RUNNER_OS} == 'Linux' ]]; then
-  # BMI
-  cd $HOME && wget --no-check-certificate http://xgitlab.cels.anl.gov/sds/bmi/-/archive/${BMI_VERSION}/bmi-${BMI_VERSION}.tar.bz2;
-  tar -xjf bmi-${BMI_VERSION}.tar.bz2;
-  # if [[ $OS_NAME == 'osx' ]]; then
-  #    patch -p1 < ${TRAVIS_BUILD_DIR}/Testing/script/bmi_osx.patch
-  # fi
-  cd bmi-${BMI_VERSION} && ./prepare && ./configure --enable-shared --disable-static --enable-bmi-only --prefix=$PREFIX && make -j2 -s && make install;
+# BMI
+cd $HOME && wget --no-check-certificate http://xgitlab.cels.anl.gov/sds/bmi/-/archive/${BMI_VERSION}/bmi-${BMI_VERSION}.tar.bz2;
+tar -xjf bmi-${BMI_VERSION}.tar.bz2;
+cd bmi-${BMI_VERSION} && ./prepare && ./configure --enable-shared --disable-static --enable-bmi-only --prefix=$PREFIX && make -j2 -s && make install;
 
-  # MPI
-  cd $HOME && wget http://www.mpich.org/static/downloads/${MPI_VERSION}/mpich-${MPI_VERSION}.tar.gz;
-  tar -xzf mpich-${MPI_VERSION}.tar.gz;
-  cd mpich-${MPI_VERSION} && ./configure --disable-fortran --with-device=ch3 --prefix=$PREFIX && make -j2 -s && make install;
+# MPI
+#cd $HOME && wget http://www.mpich.org/static/downloads/${MPI_VERSION}/mpich-${MPI_VERSION}.tar.gz;
+#tar -xzf mpich-${MPI_VERSION}.tar.gz;
+#cd mpich-${MPI_VERSION} && ./configure --disable-fortran --with-device=ch3 --prefix=$PREFIX && make -j2 -s && make install;
 
-  # OFI
-  if [ -z "$OFI_PR" ]; then
-    cd $HOME && wget https://github.com/ofiwg/libfabric/releases/download/v${OFI_VERSION}/libfabric-${OFI_VERSION}.tar.bz2
-    tar -xjf libfabric-${OFI_VERSION}.tar.bz2;
-    cd libfabric-${OFI_VERSION};
-    #wget https://github.com/ofiwg/libfabric/pull/6509.patch
-    #patch -p1 < 6509.patch
-  else
-    git clone https://github.com/ofiwg/libfabric.git libfabric-${OFI_VERSION};
-    cd libfabric-${OFI_VERSION};
-    git fetch origin pull/${OFI_PR}/head:ofi_pr;
-    git checkout ofi_pr;
-    ./autogen.sh;
-  fi
-  ./configure --prefix=$PREFIX --disable-usnic --disable-mrail --disable-rstream --disable-perf --disable-efa --disable-psm2 --disable-psm --disable-verbs --disable-shm --disable-static --disable-silent-rules ${OFI_EXTRA_FLAGS} CC="${CC}" CFLAGS="${OFI_CFLAGS}" && make -j2 -s && make install;
+# OFI
+if [ -z "$OFI_PR" ]; then
+  cd $HOME && wget https://github.com/ofiwg/libfabric/releases/download/v${OFI_VERSION}/libfabric-${OFI_VERSION}.tar.bz2
+  tar -xjf libfabric-${OFI_VERSION}.tar.bz2;
+  cd libfabric-${OFI_VERSION};
+  #wget https://github.com/ofiwg/libfabric/pull/6509.patch
+  #patch -p1 < 6509.patch
+else
+  git clone https://github.com/ofiwg/libfabric.git libfabric-${OFI_VERSION};
+  cd libfabric-${OFI_VERSION};
+  git fetch origin pull/${OFI_PR}/head:ofi_pr;
+  git checkout ofi_pr;
+  ./autogen.sh;
 fi
+./configure --prefix=$PREFIX --disable-usnic --disable-mrail --disable-rstream --disable-perf --disable-efa --disable-psm2 --disable-psm --disable-verbs --disable-shm --disable-static --disable-silent-rules ${OFI_EXTRA_FLAGS} CC="${CC}" CFLAGS="${OFI_CFLAGS}" && make -j2 -s && make install;
