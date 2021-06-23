bf01345ee0dbbab0425759a6d973589cf4eac3ad
Jerome Soumagne <jsoumagne@hdfgroup.org>
2021-06-23 16:17:32 -0500

    NA: add na_cb_type_to_string() and clean up cb type enum

--- src/na/na.c
+++ src/na/na.c
@@ -118,6 +118,11 @@ static const struct na_class_ops *const na_class_table[] = {
 static const char *const na_return_name[] = {NA_RETURN_VALUES};
 #undef X
 
+/* Callback type string table */
+#define X(a) #a,
+static const char *const na_cb_type_name[] = {NA_CB_TYPES};
+#undef X
+
 /* NA_LOG_DEBUG_LESIZE: default number of debug log entries. */
 #define NA_LOG_DEBUG_LESIZE (256)
 
@@ -1433,6 +1438,13 @@ const char *
 NA_Error_to_string(na_return_t errnum)
 {
     return na_return_name[errnum];
+}
+
+/*---------------------------------------------------------------------------*/
+const char *
+na_cb_type_to_string(na_cb_type_t cb_type)
+{
+    return na_cb_type_name[cb_type];
 }
 
 /*---------------------------------------------------------------------------*/
