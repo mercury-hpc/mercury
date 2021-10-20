2a345cd926e440481caeb77bbd2d8748e3c8141b
Jerome Soumagne <jsoumagne@hdfgroup.org>
2021-10-19 19:29:22 -0500

    CI: add intel oneAPI compilers to CI matrix
    Update CI scripts

--- Testing/script/gh_ctest.sh
+++ Testing/script/gh_ctest.sh
@@ -30,6 +30,12 @@ else
   BUILD_SHARED=TRUE
 fi
 
+# Source intel env when using icc
+if [[ ${CC} == 'icc' ]]; then
+  ICC_LATEST_VERSION=$(ls -1 /opt/intel/oneapi/compiler/ | grep -v latest | sort | tail -1)
+  source /opt/intel/oneapi/compiler/"$ICC_LATEST_VERSION"/env/vars.sh
+fi
+
 export COV=`which gcov`
 
 export DEPS_PREFIX=${RUNNER_TEMP}/${INSTALL_DIR}
