7da681c8943db7fa769eb139a27307cef1692497
Jerome Soumagne <jsoumagne@hdfgroup.org>
2021-11-03 13:55:26 -0500

    CI: update libfabric to 1.14.0

--- Testing/script/gh_install_deps.sh
+++ Testing/script/gh_install_deps.sh
@@ -10,7 +10,7 @@ if [[ $MERCURY_BUILD_CONFIGURATION == 'Debug' ]]; then
   OFI_EXTRA_FLAGS="--enable-debug"
 fi
 #OFI_PR=
-OFI_VERSION=1.13.2
+OFI_VERSION=1.14.0
 
 # UCX
 if [[ $MERCURY_BUILD_CONFIGURATION == 'Tsan' ]]; then
