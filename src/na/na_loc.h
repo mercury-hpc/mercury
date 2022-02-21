f006e24adb6f521165affac586d9d4e0efe49756
Jerome Soumagne <jsoumagne@hdfgroup.org>
2022-02-21 17:01:21 -0600

    NA OFI: add na_loc module and NA_OFI_USE_HWLOC option (fix #540)
    Use hwloc to select domain to use if NIC information is available
    
    Tidy up addr_format check
    
    Add NA_OFI_LOC_INFO flag for providers that support loc detection

---
+++ src/na/na_loc.h
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2013-2021 UChicago Argonne, LLC and The HDF Group.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef NA_LOC_H
+#define NA_LOC_H
+
+#include "na_types.h"
+
+/*************************************/
+/* Public Type and Struct Definition */
+/*************************************/
+
+struct na_loc_info;
+
+/*****************/
+/* Public Macros */
+/*****************/
+
+/*********************/
+/* Public Prototypes */
+/*********************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Init loc info. Must be freed with na_loc_info_destroy().
+ *
+ * \param na_loc_info_p [OUT]   pointer to returned loc info
+ *
+ * \return NA_SUCCESS or corresponding NA error code
+ */
+NA_PRIVATE na_return_t
+na_loc_info_init(struct na_loc_info **na_loc_info_p);
+
+/**
+ * Free loc info.
+ *
+ * \param na_loc_info [IN/OUT]  pointer to loc info
+ */
+NA_PRIVATE void
+na_loc_info_destroy(struct na_loc_info *na_loc_info);
+
+/**
+ * Check if a process and a pci device share the same cpuset.
+ *
+ * \param na_loc_info [IN]      pointer to loc info
+ * \param domain_id  [IN]       PCI domain ID
+ * \param bus_id  [IN]          PCI bus ID
+ * \param device_id  [IN]       PCI device ID
+ * \param function_id  [IN]     PCI function ID
+ *
+ * \return NA_TRUE if they share the same cpu set, NA_FALSE otherwise
+ */
+NA_PRIVATE na_bool_t
+na_loc_check_pcidev(struct na_loc_info *na_loc_info, unsigned int domain_id,
+    unsigned int bus_id, unsigned int device_id, unsigned int function_id);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NA_LOC_H */
