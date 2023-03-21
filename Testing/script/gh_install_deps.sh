320724a8d44001cd3319f579e6d41f437ba7afd9
Jerome Soumagne <jerome.soumagne@intel.com>
2023-03-21 12:16:06 -0500

    CI: update to OFI 1.18.0rc1

--- Testing/script/gh_install_deps.sh
+++ Testing/script/gh_install_deps.sh
@@ -10,7 +10,7 @@ if [[ $MERCURY_BUILD_CONFIGURATION == 'Debug' ]]; then
   OFI_EXTRA_FLAGS="--enable-debug"
 fi
 #OFI_PR=
-OFI_VERSION=1.17.0
+OFI_VERSION=1.18.0rc1
 
 # UCX
 if [[ $MERCURY_BUILD_CONFIGURATION == 'Tsan' ]]; then
