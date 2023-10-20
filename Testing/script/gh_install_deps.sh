170fee63856cb0215c6e415ea258aebe2925a85f
Jerome Soumagne <jerome.soumagne@intel.com>
2023-10-20 11:54:25 -0500

    CI: update to OFI 1.19.0 and UCX 1.15.0
    Add rxm to ofi test protocols
    
    Add all to ucx test protocols

--- Testing/script/gh_install_deps.sh
+++ Testing/script/gh_install_deps.sh
@@ -10,7 +10,7 @@ if [[ $MERCURY_BUILD_CONFIGURATION == 'Debug' ]]; then
   OFI_EXTRA_FLAGS="--enable-debug"
 fi
 #OFI_PR=
-OFI_VERSION=1.19.0rc2
+OFI_VERSION=1.19.0
 
 # UCX
 if [[ $MERCURY_BUILD_CONFIGURATION == 'Tsan' ]]; then
@@ -20,7 +20,7 @@ fi
 if [[ $MERCURY_BUILD_CONFIGURATION == 'Debug' ]]; then
   UCX_EXTRA_FLAGS="--enable-debug"
 fi
-UCX_VERSION=1.14.1
+UCX_VERSION=1.15.0
 
 PREFIX=${RUNNER_TEMP}/${INSTALL_DIR}
 
