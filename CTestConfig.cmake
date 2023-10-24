f0586f61622e898fb1e49db6d5c54ae4ccb295da
Jerome Soumagne <jerome.soumagne@intel.com>
2023-10-24 17:39:58 -0500

    CI: update CTest site

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,15 +1,15 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to use Dart and the CDash dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
 ##   ENABLE_TESTING()
-##   INCLUDE(Dart)
+##   INCLUDE(CTest)
 
-set(CTEST_PROJECT_NAME "MERCURY")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 CST")
+set(CTEST_PROJECT_NAME Mercury)
+set(CTEST_NIGHTLY_START_TIME 06:00:00 UTC)
 
-set(CTEST_DROP_METHOD "https")
-set(CTEST_DROP_SITE "mercury-cdash.hdfgroup.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=Mercury")
+set(CTEST_SUBMIT_URL https://cdash.mercury.daos.io/submit.php?project=Mercury)
+
 set(CTEST_DROP_SITE_CDASH TRUE)
 
