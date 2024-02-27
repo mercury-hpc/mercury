ef368c8dfc2b4ca941e5b6a29ab2fb3049ed7ab0
Jerome Soumagne <jerome.soumagne@intel.com>
2024-02-27 11:49:50 -0600

    CI: update libfabric to 1.20.1

--- Testing/script/gh_install_deps.sh
+++ Testing/script/gh_install_deps.sh
@@ -10,7 +10,7 @@ if [[ $MERCURY_BUILD_CONFIGURATION == 'Debug' ]]; then
   OFI_EXTRA_FLAGS="--enable-debug"
 fi
 #OFI_PR=
-OFI_VERSION=1.20.0
+OFI_VERSION=1.20.1
 
 # UCX
 if [[ $MERCURY_BUILD_CONFIGURATION == 'Tsan' ]]; then
