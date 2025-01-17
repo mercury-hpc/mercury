e315742a0ccafffa55c6345f9b00db28625a4cff
Jerome Soumagne <jerome.soumagne@hpe.com>
2025-01-17 17:30:39 -0600

    CI: fix clang-format version
    No longer need to install pkg-config on macOS
    
    Install libhwloc-dev and libtirpc-dev on ubuntu
    
    Build mpich manually to prevent ucx/ofi dependencies
    
    Disable ucx/mpi/psm builds when non needed

--- Testing/script/gh_install_deps.sh
+++ Testing/script/gh_install_deps.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 
-BMI_VERSION=2.8.1
-#MPI_VERSION=3.4.1
-if [[ $MERCURY_BUILD_CONFIGURATION == 'Tsan' ]]; then
-  OFI_CFLAGS="-O1 -g -fsanitize=thread"
-  OFI_EXTRA_FLAGS="--enable-debug"
-fi
-if [[ $MERCURY_BUILD_CONFIGURATION == 'Debug' ]]; then
-  OFI_EXTRA_FLAGS="--enable-debug"
-fi
+# Versions
+BMI_VERSION=latest
+MPI_VERSION=4.2.3
 #OFI_PR=
 OFI_VERSION=1.22.0
-
-# UCX
-if [[ $MERCURY_BUILD_CONFIGURATION == 'Tsan' ]]; then
-  UCX_CFLAGS="-O1 -g -fsanitize=thread"
-  UCX_EXTRA_FLAGS="--enable-debug"
-fi
-if [[ $MERCURY_BUILD_CONFIGURATION == 'Debug' ]]; then
-  UCX_EXTRA_FLAGS="--enable-debug"
-fi
+# PSM_VERSION=updates
 UCX_VERSION=1.17.0
 
-PREFIX=${RUNNER_TEMP}/${INSTALL_DIR}
-
-# PSM
-if [[ $MERCURY_BUILD_CONFIGURATION == 'Tsan' ]]; then
-  PSM_EXTRA_FLAGS="PSM_DEBUG=1 PSM_SANITIZE=1"
-fi
 if [[ $MERCURY_BUILD_CONFIGURATION == 'Debug' ]]; then
-  PSM_EXTRA_FLAGS="PSM_DEBUG=1"
+  OFI_EXTRA_FLAGS="--enable-debug"
+  # PSM_EXTRA_FLAGS="PSM_DEBUG=1"
+  UCX_EXTRA_FLAGS="--enable-debug"
+elif [[ $MERCURY_BUILD_CONFIGURATION == 'Asan' ]]; then
+    BUILD_MPI=0
+elif [[ $MERCURY_BUILD_CONFIGURATION == 'Tsan' ]]; then
+      BUILD_MPI=0
+      BUILD_UCX=0
+      OFI_CFLAGS="-O1 -g -fsanitize=thread"
+      OFI_EXTRA_FLAGS="--enable-debug"
+      # PSM_EXTRA_FLAGS="PSM_DEBUG=1 PSM_SANITIZE=1"
+      UCX_CFLAGS="-O1 -g -fsanitize=thread"
+      UCX_EXTRA_FLAGS="--enable-debug"
 fi
-PSM_VERSION=updates
+
+PREFIX=${RUNNER_TEMP}/${INSTALL_DIR}
 
 set -e
 
@@ -39,17 +32,12 @@ set -e
 if [[ ${CC} == 'icx' ]]; then
   CC=gcc
   CXX=g++
+  BUILD_MPI=0
 fi
 
 # BMI
-cd $HOME && wget --secure-protocol=TLSv1_2 https://github.com/radix-io/bmi/archive/refs/tags/v${BMI_VERSION}.tar.gz -O bmi-${BMI_VERSION}.tar.gz;
-tar -xzf bmi-${BMI_VERSION}.tar.gz;
+cd $HOME && git clone https://github.com/radix-io/bmi.git bmi-${BMI_VERSION};
 cd bmi-${BMI_VERSION} && ./prepare && ./configure --enable-shared --disable-static --enable-bmi-only --prefix=$PREFIX && make -j2 -s && make install;
-
-# MPI
-#cd $HOME && wget http://www.mpich.org/static/downloads/${MPI_VERSION}/mpich-${MPI_VERSION}.tar.gz;
-#tar -xzf mpich-${MPI_VERSION}.tar.gz;
-#cd mpich-${MPI_VERSION} && ./configure --disable-fortran --with-device=ch3 --prefix=$PREFIX && make -j2 -s && make install;
 
 # OFI
 if [ -z "$OFI_PR" ]; then
@@ -69,15 +57,24 @@ fi
 ./configure --prefix=$PREFIX --disable-usnic --disable-mrail --disable-rstream --disable-perf --disable-efa --disable-psm2 --disable-psm --disable-opx --disable-dmabuf_peer_mem --disable-hook_hmem --disable-hook_debug --disable-rxd --disable-udp --disable-verbs --disable-shm --disable-static --disable-silent-rules ${OFI_EXTRA_FLAGS} CC="${CC}" CFLAGS="${OFI_CFLAGS}" && make -j2 -s && make install;
 
 if [[ ${RUNNER_OS} == 'Linux' ]]; then
-  # UCX
-  cd $HOME && wget --secure-protocol=TLSv1_2 https://github.com/openucx/ucx/releases/download/v${UCX_VERSION}/ucx-${UCX_VERSION}.tar.gz
-  tar -xzf ucx-${UCX_VERSION}.tar.gz;
-  cd ucx-${UCX_VERSION};
-  ./configure --prefix=$PREFIX --enable-profiling --enable-frame-pointer --enable-stats --enable-memtrack --enable-fault-injection --enable-mt --disable-numa --without-java --without-go --disable-silent-rules ${UCX_EXTRA_FLAGS} CC="${CC}" CXX="${CXX}" CFLAGS="${UCX_CFLAGS}" && make -j2 -s && make install;
+  if [[ ${BUILD_MPI} != '0' ]]; then
+    # MPI
+    cd $HOME && wget http://www.mpich.org/static/downloads/${MPI_VERSION}/mpich-${MPI_VERSION}.tar.gz;
+    tar -xzf mpich-${MPI_VERSION}.tar.gz;
+    cd mpich-${MPI_VERSION} && ./configure --enable-lib-depend --disable-rpath --disable-silent-rules --disable-dependency-tracking --disable-fortran --enable-shared --enable-static=no --with-pm=hydra:gforker --with-libfabric=$PREFIX --with-hwloc --prefix=$PREFIX && make -j2 -s && make install;
+  fi
+
+  if [[ ${BUILD_UCX} != '0' ]]; then
+    # UCX
+    cd $HOME && wget --secure-protocol=TLSv1_2 https://github.com/openucx/ucx/releases/download/v${UCX_VERSION}/ucx-${UCX_VERSION}.tar.gz
+    tar -xzf ucx-${UCX_VERSION}.tar.gz;
+    cd ucx-${UCX_VERSION};
+    ./configure --prefix=$PREFIX --enable-profiling --enable-frame-pointer --enable-stats --enable-memtrack --enable-fault-injection --enable-mt --disable-numa --without-java --without-go --disable-silent-rules ${UCX_EXTRA_FLAGS} CC="${CC}" CXX="${CXX}" CFLAGS="${UCX_CFLAGS}" && make -j2 -s && make install;
+  fi
 
   # PSM
-  cd $HOME && git clone https://github.com/mercury-hpc/psm.git -b ${PSM_VERSION} psm-${PSM_VERSION}
-  cd psm-${PSM_VERSION}
-  make install DESTDIR=$PREFIX ${PSM_EXTRA_FLAGS}
+  # cd $HOME && git clone https://github.com/mercury-hpc/psm.git -b ${PSM_VERSION} psm-${PSM_VERSION}
+  # cd psm-${PSM_VERSION}
+  # make install DESTDIR=$PREFIX ${PSM_EXTRA_FLAGS}
 fi
 
