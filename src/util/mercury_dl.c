6e0f7a0ca715323baef6bfad1480d685c9cb3d70
Jerome Soumagne <jerome.soumagne@hpe.com>
2025-04-18 17:21:00 -0500

    NA: remove NA_DEFAULT_PLUGIN_PATH and use NA_PLUGIN_RELATIVE_PATH
    Clean up previous commit and use cmake_path()
    
    Fix plugin scan to continue if one plugin cannot load

---
+++ src/util/mercury_dl.c
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2013-2022 UChicago Argonne, LLC and The HDF Group.
+ * Copyright (c) 2022-2023 Intel Corporation.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#if !defined(_WIN32) && !defined(_GNU_SOURCE)
+#    define _GNU_SOURCE
+#endif
+#include "mercury_dl.h"
+
+#include <string.h>
+
+/*---------------------------------------------------------------------------*/
+int
+hg_dl_get_path(const void *addr, char *path, size_t path_size)
+{
+#ifdef _WIN32
+    HMODULE module;
+    if (GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+                               GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+            (LPCSTR) addr, &module))
+        return GetModuleFileNameA(module, path, (DWORD) path_size);
+#else
+    Dl_info info;
+    if (dladdr(addr, &info) && info.dli_fname) {
+        strncpy(path, info.dli_fname, path_size);
+        path[path_size - 1] = '\0';
+        return HG_UTIL_SUCCESS;
+    }
+#endif
+
+    return HG_UTIL_FAIL;
+}
