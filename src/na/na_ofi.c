9818dded6009bda7f06a2e8dafa066e9c2cda5c2
Ilia Sotnikov <isotnikov@vdura.com>
2025-09-08 10:42:50 +0300

    NA OFI, HG mem: fix compilation under FreeBSD
    The change addresses certain macros not being defined under FreeBSD,
    such as `MAP_HUGETLB` for `mmap()`
    
    Signed-off-by: Ilia Sotnikov <isotnikov@vdura.com>

--- src/na/na_ofi.c
+++ src/na/na_ofi.c
@@ -2,6 +2,7 @@
  * Copyright (c) 2013-2022 UChicago Argonne, LLC and The HDF Group.
  * Copyright (c) 2017-2024 Intel Corporation.
  * Copyright (c) 2024-2025 Hewlett Packard Enterprise Development LP.
+ * Copyright (c) 2025 VDURA, Inc.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -2218,7 +2219,7 @@ na_ofi_errno_to_na(int rc)
             ret = NA_INTERRUPT;
             break;
         case FI_EIO:
-#if !defined(__APPLE__)
+#if !defined(__APPLE__) && !defined(__FreeBSD__)
         case FI_EREMOTEIO:
 #endif
             ret = NA_IO_ERROR;
@@ -2236,7 +2237,7 @@ na_ofi_errno_to_na(int rc)
             ret = NA_NOMEM;
             break;
         case FI_EACCES:
-#if !defined(_WIN32) && !defined(__APPLE__)
+#if !defined(_WIN32) && !defined(__APPLE__) && !defined(__FreeBSD__)
         case FI_EKEYREJECTED:
 #endif
             ret = NA_ACCESS;
@@ -2297,7 +2298,9 @@ na_ofi_errno_to_na(int rc)
             ret = NA_CANCELED;
             break;
         case FI_ENOMSG:
+#if !defined(__FreeBSD__)
         case FI_ENODATA:
+#endif
         /* In practice the following codes are not errors but treat them as is
          * in this routine. */
         case FI_EISCONN:
